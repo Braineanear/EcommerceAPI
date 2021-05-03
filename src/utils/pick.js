1303414bbdab7c5a02065546d59773ef941706fd
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-03 03:45:25 +0200

    :card_file_box: Create an object composed of the picked object properties

---
+++ src/utils/pick.js
@@ -0,0 +1,15 @@
+/**
+ * Create an object composed of the picked object properties
+ * @param {Object} object
+ * @param {string[]} keys
+ * @returns {Object}
+ */
+const pick = (object, keys) =>
+  keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      obj[key] = object[key];
+    }
+    return obj;
+  }, {});
+
+export default pick;
