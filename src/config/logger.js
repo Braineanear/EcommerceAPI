bf2eaa9ddcf3b79cc4e24fd80366063fbfe62490
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-03 03:47:03 +0200

    :wrench: Add Error Logger Config File

---
+++ src/config/logger.js
@@ -0,0 +1,25 @@
+import winston from 'winston';
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    winston.format.colorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error']
+    })
+  ]
+});
+
+export default logger;
