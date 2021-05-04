1954f1deb391a15ef458a0d898d790fb5c48520e
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-04 03:17:52 +0200

    :bug: Fixing Async Function (Catching Errors)

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,22 +4,26 @@ import catchAsync from '../utils/catchAsync';
 import config from './config';
 import tokenTypes from './tokens';
 import { User } from '../models/index';
+import logger from './logger';
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 };
 
-const jwtVerify = catchAsync(async (payload, done) => {
+const jwtVerify = async (payload, done) => {
   if (payload.type !== tokenTypes.ACCESS) {
     throw new Error('Invalid token type');
   }
-  const user = await User.findById(payload.sub);
+  const [err, user] = await catchAsync(User.findById(payload.sub));
   if (!user) {
     return done(null, false);
   }
+  if (err) {
+    logger.error(err);
+  }
   done(null, user);
-});
+};
 
 const jwtLogin = new JWTStrategy(jwtOptions, jwtVerify);
 
