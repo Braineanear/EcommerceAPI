1bdfc5ac0ffdf4a988854cd3d37e1cc9f1f0f3c0
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-04 02:45:51 +0200

    :wrench: Update Catch Async Util

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,5 +1,38 @@
-const catchAsync = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+// Native Error Types
+const nativeExceptions = [
+  EvalError,
+  RangeError,
+  ReferenceError,
+  SyntaxError,
+  TypeError,
+  URIError
+].filter((except) => typeof except === 'function');
+
+// Throw Native Errors
+const throwNative = (error) => {
+  for (const Exception of nativeExceptions) {
+    if (error instanceof Exception) throw error;
+  }
 };
+
+// Helper Buddy For Removing async/await try/catch Litter
+const catchAsync = (promise, finallyFunc) =>
+  promise
+    .then((data) => {
+      if (data instanceof Error) {
+        throwNative(data);
+        return [data];
+      }
+      return [undefined, data];
+    })
+    .catch((error) => {
+      throwNative(error);
+      return [error];
+    })
+    .finally(() => {
+      if (finallyFunc && typeof finallyFunc === 'function') {
+        finallyFunc();
+      }
+    });
 
 export default catchAsync;
