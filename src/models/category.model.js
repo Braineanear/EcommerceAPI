ea9165a30ad3990b6094431b283515c6f8219f34
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-06 21:38:45 +0200

    :sparkles: Add Category Model

---
+++ src/models/category.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { toJSON, paginate } from './plugins/index';
+
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    imageId: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// add plugin that converts mongoose to json
+categorySchema.plugin(toJSON);
+categorySchema.plugin(paginate);
+
+const Category = mongoose.model('Category', categorySchema);
+
+export default Category;
