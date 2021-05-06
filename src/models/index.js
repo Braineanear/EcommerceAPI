ff471bee9d90ec67b2fa0d5eafeeb5735fc85de1
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-06 21:39:30 +0200

    :sparkles: Update Index

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,7 @@
 import Token from './token.model';
 import User from './user.model';
+import Product from './product.model';
+import Category from './category.model';
+import Order from './order.model';
 
-export { Token, User };
+export { Token, User, Product, Category, Order };
