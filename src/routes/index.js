1e6417188bad01d21cd5a61c388b7d3694d528fd
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-06 21:35:02 +0200

    :sparkles: Update Index Main

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 
 import userRoute from './user.route';
 import authRoute from './auth.route';
+import productRoute from './product.route';
+import categoryRoute from './category.route';
 
 const router = express.Router();
 
 router.use('/auth', authRoute);
 router.use('/user', userRoute);
+router.use('/product', productRoute);
+router.use('/category', categoryRoute);
 
 export default router;
