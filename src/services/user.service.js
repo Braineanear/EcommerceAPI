f24b67a6d24bfc5f6332b7005970887277edc7a9
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-07 03:46:38 +0200

    :fire: Fix CatchAsync

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -10,32 +10,21 @@ import { User } from '../models/index';
  * @param {Object} userBody
  * @returns {Promise<User>}
  */
-export const createUser = async (userBody) => {
-  let err, isEmailTaken, user;
-
+export const createUser = catchAsync(async (userBody) => {
   // 1) Check if The Email Already Taken
-  [err, isEmailTaken] = await catchAsync(User.isEmailTaken(userBody.email));
+  const isEmailTaken = await User.isEmailTaken(userBody.email);
 
-  // 2) Handling isEmailTaken Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-
-  // 3) If The Email Taken
+  // 2) If The Email Taken
   if (isEmailTaken) {
     throw new AppError('Email already taken', 400);
   }
 
-  // 4) Create New User
-  [err, user] = await catchAsync(User.create(userBody));
+  // 3) Create New User
+  const user = await User.create(userBody);
 
-  // 5) Handling Create Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-
+  // 4) If Everything is OK, Send User Data
   return user;
-};
+});
 
 /**
  * Query for users
@@ -46,23 +35,18 @@ export const createUser = async (userBody) => {
  * @param {number} [options.page] - Current page (default = 1)
  * @returns {Promise<QueryResult>}
  */
-export const queryUsers = async (filter, options) => {
+export const queryUsers = catchAsync(async (filter, options) => {
   // 1) Get All Users
-  const [err, users] = await catchAsync(User.paginate(filter, options));
+  const users = await User.paginate(filter, options);
 
   // 2) Check if No Users
   if (!users) {
     throw new AppError('No Users Found', 404);
   }
 
-  // 3) Handling Paginate Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-
-  // 4) If Everything is OK, Send Users Data
+  // 3) If Everything is OK, Send Users Data
   return users;
-};
+});
 
 /**
  * Update user by id
@@ -70,62 +54,41 @@ export const queryUsers = async (filter, options) => {
  * @param {Object} updateBody
  * @returns {Promise<User>}
  */
-export const updateUserById = async (userId, updateBody) => {
-  let err, isEmailTaken, user;
-
+export const updateUserById = catchAsync(async (userId, updateBody) => {
   // 1) Check if Email Already Taken
-  [err, isEmailTaken] = await catchAsync(
-    User.isEmailTaken(updateBody.email, userId)
-  );
+  const isEmailTaken = await User.isEmailTaken(updateBody.email, userId);
 
-  // 2) Handling isEmailTaken Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-
-  // 3) If Email Taken
+  // 2) If Email Taken
   if (updateBody.email && isEmailTaken) {
     throw new AppError('Email already taken', 400);
   }
 
-  // 4) Find User Document and Update it
-  [err, user] = await catchAsync(
-    User.findByIdAndUpdate(userId, updateBody, {
-      new: true,
-      runValidators: true
-    })
-  );
+  // 3) Find User Document and Update it
+  const user = await User.findByIdAndUpdate(userId, updateBody, {
+    new: true,
+    runValidators: true
+  });
 
-  // 5) Check if User Already Exist
+  // 4) Check if User Already Exist
   if (!user) {
     throw new AppError('User not found', 404);
   }
 
-  // 6) Handling findByIdAndUpdate Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-
-  // 7) If Everything is OK, Send User Data
+  // 5) If Everything is OK, Send User Data
   return user;
-};
+});
 
 /**
  * Delete user by id
  * @param {ObjectId} userId
  * @returns {Promise<User>}
  */
-export const deleteUserById = async (userId) => {
+export const deleteUserById = catchAsync(async (userId) => {
   // 1) Find User Document and Delete it
-  const [err, user] = await catchAsync(User.findByIdAndDelete(userId));
+  const user = await User.findByIdAndDelete(userId);
 
   // 2) Check if User Already Exist
   if (!user) {
     throw new AppError('User not found', 404);
   }
-
-  // 3) Handling findByIdAndDelete Method Errors
-  if (err) {
-    throw new AppError(err, 500);
-  }
-};
+});
