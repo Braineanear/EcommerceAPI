a227d0fe67f84acf78787e04382cfdba1d866be6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-07 03:52:08 +0200

    :fire: Fix CatchAsync

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,26 +4,24 @@ import catchAsync from '../utils/catchAsync';
 import config from './config';
 import tokenTypes from './tokens';
 import { User } from '../models/index';
-import logger from './logger';
 
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 };
 
-const jwtVerify = async (payload, done) => {
+const jwtVerify = catchAsync(async (payload, done) => {
   if (payload.type !== tokenTypes.ACCESS) {
     throw new Error('Invalid token type');
   }
-  const [err, user] = await catchAsync(User.findById(payload.sub));
+
+  const user = await User.findById(payload.sub);
+
   if (!user) {
     return done(null, false);
   }
-  if (err) {
-    logger.error(err);
-  }
   done(null, user);
-};
+});
 
 const jwtLogin = new JWTStrategy(jwtOptions, jwtVerify);
 
