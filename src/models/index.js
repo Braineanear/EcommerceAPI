d0224c51cb91560dd4ce9398c97fa7b30f11bbaa
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-08 23:35:16 +0200

    :sparkles: Add Products Model

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,5 @@ import Token from './token.model';
 import User from './user.model';
 import Product from './product.model';
 import Category from './category.model';
-import Order from './order.model';
 
-export { Token, User, Product, Category, Order };
+export { Token, User, Product, Category };
