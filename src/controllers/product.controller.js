80eb7e57a01ac3af87d385e6adbdc55ee3acab85
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-12 22:59:54 +0200

    :sparkles: Add Product Updates & Delete

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,19 +1,55 @@
+// Config
+import logger from '../config/logger';
+
 // Utils
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
-import dataUri from '../utils/datauri';
-import { uploadFile, destroyFile } from '../utils/cloudinary';
 
-// Model
-import { Product } from '../models/index';
+// Services
+import { productService, redisService } from '../services/index';
 
 export const getAllProducts = catchAsync(async (req, res, next) => {
-  const products = await Product.find({}).populate('category', 'name');
+  let { page, sortBy, limit } = req.query;
+  // 1) Setting Default Params
+  if (!page) page = 1;
+  if (!sortBy) sortBy = 'sortField:asc';
+  if (!limit) limit = 10;
 
+  // 2) Generating Redis key
+  const key = redisService.generateCacheKey(
+    'products',
+    `page:${page}-sortBy:${sortBy}-limit:${limit}`
+  );
+
+  // 3) Getting Cached Data From Redis
+  let products = await redisService.get(key);
+
+  // 4) Check If Cached Data Already Exist
+  if (!products) {
+    logger.error('No Caching Data');
+  }
+
+  products = JSON.parse(products);
+
+  // 5) If Cached Data Exit Return it
+  if (products) {
+    return res.status(200).json({
+      products
+    });
+  }
+
+  // 7) Get All Users
+  products = await productService.queryProducts(req);
+
+  // 8) Check If Products Already Exist
   if (!products) {
     return next(new AppError('No Products Found', 404));
   }
 
+  // 9) Put Data into Redis With a Specific Key
+  redisService.set(key, JSON.stringify(products), 'EX', 60);
+
+  // 10) If Everything is OK, Send Data
   return res.status(200).json({
     status: 'success',
     message: 'Found Products',
@@ -23,12 +59,38 @@ export const getAllProducts = catchAsync(async (req, res, next) => {
 
 export const getProduct = catchAsync(async (req, res, next) => {
   const { id } = req.params;
-  const product = await Product.findById(id).populate('user');
+  // 1) Generating Redis key
+  const key = redisService.generateCacheKey('product', `id:${id}`);
 
+  // 2) Getting Cached Data From Redis
+  let product = await redisService.get(key);
+
+  // 3) Check If Cached Data Already Exist
+  if (!product) {
+    logger.error('No Caching Data');
+  }
+
+  product = JSON.parse(product);
+
+  // 4) If Cached Data Exit Return it
+  if (product) {
+    return res.status(200).json({
+      product
+    });
+  }
+
+  // 5) Get Product Using It's ID
+  product = await productService.queryProductById(id);
+
+  // 6) Check If Product Already Exist
   if (!product) {
     return next(new AppError(`No Product Found With This ID: ${id}`, 404));
   }
 
+  // 7) Put Data into Redis With a Specific Key
+  redisService.set(key, JSON.stringify(product), 'EX', 60);
+
+  // 8) If Everything is OK, Send Product
   return res.status(200).json({
     status: 'success',
     message: 'Found Product',
@@ -37,202 +99,78 @@ export const getProduct = catchAsync(async (req, res, next) => {
 });
 
 export const addProduct = catchAsync(async (req, res, next) => {
-  const {
-    name,
-    description,
-    category,
-    price,
-    oldPrice,
-    color,
-    size,
-    quantity,
-    quality,
-    isPopular,
-    sold,
-    offer,
-    status,
-    isOutOfStock
-  } = req.body;
+  // 1) Create Product
+  const products = await productService.createProduct(req);
 
-  const mainImage = req.files.filter(
-    (image) => image.fieldname === 'mainImage'
-  );
-  const images = req.files.filter((image) => image.fieldname === 'images');
-
-  const folderName = `Products/${name.trim().split(' ').join('')}`;
-  const imagesLink = [];
-  const imagesId = [];
-
-  if (
-    !name ||
-    !description ||
-    !category ||
-    !price ||
-    !oldPrice ||
-    !color ||
-    !size ||
-    !quantity ||
-    !quality ||
-    !isPopular ||
-    !sold ||
-    !offer ||
-    !status ||
-    !isOutOfStock ||
-    mainImage.length === 0 ||
-    images.length === 0
-  ) {
-    return next(new AppError('All fields are required', 400));
+  // 2) Check If Products Already Exist
+  if (!products) {
+    return next(new AppError('No Products Found', 404));
   }
 
-  const imagesPromises = images.map((image) =>
-    uploadFile(dataUri(image).content, folderName)
-  );
-
-  const imagesResult = await Promise.all(imagesPromises);
-  const imageResult = await uploadFile(
-    dataUri(mainImage[0]).content,
-    folderName
-  );
-
-  imagesResult.forEach((image) => {
-    imagesLink.push(image.secure_url);
-    imagesId.push(image.public_id);
-  });
-
-  let result = await Product.create({
-    mainImage: imageResult.secure_url,
-    mainImageId: imageResult.public_id,
-    images: imagesLink,
-    imagesId,
-    name,
-    description,
-    category,
-    price,
-    oldPrice,
-    color,
-    size,
-    user: req.user._id,
-    quantity,
-    quality,
-    isPopular,
-    sold,
-    offer,
-    status,
-    isOutOfStock
-  });
-
-  result = await Product.findById(result._id);
-
+  // 3) If Everything is OK, Send Data
   return res.status(200).json({
     status: 'success',
     message: 'Category Created Successfully',
-    result
+    products
   });
 });
 
-export const updateProduct = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const {
-    name,
-    description,
-    category,
-    price,
-    oldPrice,
-    color,
-    size,
-    quantity,
-    quality,
-    isPopular,
-    sold,
-    offer,
-    status,
-    isOutOfStock
-  } = req.body;
+export const updateProductDetails = catchAsync(async (req, res, next) => {
+  // 1) Update Product Using It's ID
+  const product = await productService.updateProductDetails(req);
 
-  const mainImage = req.files.filter(
-    (image) => image.fieldname === 'mainImage'
-  );
-  const images = req.files.filter((image) => image.fieldname === 'images');
-
-  const folderName = `Products/${name.trim().split(' ').join('')}`;
-  const imagesLink = [];
-  const imagesId = [];
-
-  if (
-    !name ||
-    !description ||
-    !category ||
-    !price ||
-    !oldPrice ||
-    !color ||
-    !size ||
-    !quantity ||
-    !quality ||
-    !isPopular ||
-    !sold ||
-    !offer ||
-    !status ||
-    !isOutOfStock ||
-    mainImage.length === 0 ||
-    images.length === 0
-  ) {
-    return next(new AppError('All fields are required', 400));
-  }
-
-  const product = await Product.findById(id);
-
+  // 2) Check If Product Already Exist
   if (!product) {
-    return next(new AppError(`No Product Found With This ID: ${id}`, 404));
+    return next(new AppError(`No Product Found With This ID: ${req.id}`, 404));
   }
 
-  const productImagesID = product.imagesId;
-  const productMainImageID = product.mainImageId;
-
-  productImagesID.forEach((image) => destroyFile(image));
-  destroyFile(productMainImageID);
-
-  const imagesPromises = images.map((image) =>
-    uploadFile(dataUri(image).content, folderName)
-  );
-
-  const imagesResult = await Promise.all(imagesPromises);
-  const imageResult = await uploadFile(
-    dataUri(mainImage[0]).content,
-    folderName
-  );
-
-  imagesResult.forEach((image) => {
-    imagesLink.push(image.secure_url);
-    imagesId.push(image.public_id);
+  // 3) If Everything is OK, Send Product
+  return res.status(200).json({
+    status: 'success',
+    message: 'Product Updated Successfully',
+    product
   });
+});
 
-  let result = await Product.create({
-    mainImage: imageResult.secure_url,
-    mainImageId: imageResult.public_id,
-    images: imagesLink,
-    imagesId,
-    name,
-    description,
-    category,
-    price,
-    oldPrice,
-    color,
-    size,
-    user: req.user._id,
-    quantity,
-    quality,
-    isPopular,
-    sold,
-    offer,
-    status,
-    isOutOfStock
+export const updateProductMainImage = catchAsync(async (req, res, next) => {
+  // 1) Update Product Using It's ID
+  const product = await productService.updateProductMainImage(req);
+
+  // 2) Check If Product Already Exist
+  if (!product) {
+    return next(new AppError(`No Product Found With This ID: ${req.id}`, 404));
+  }
+
+  // 3) If Everything is OK, Send Product
+  return res.status(200).json({
+    status: 'success',
+    message: 'Product Updated Successfully',
+    product
   });
+});
 
-  result = await Product.findById(result._id);
+export const updateProductImages = catchAsync(async (req, res, next) => {
+  // 1) Update Product Using It's ID
+  const product = await productService.updateProductImages(req);
+
+  // 2) Check If Product Already Exist
+  if (!product) {
+    return next(new AppError(`No Product Found With This ID: ${req.id}`, 404));
+  }
+
+  // 3) If Everything is OK, Send Product
+  return res.status(200).json({
+    status: 'success',
+    message: 'Product Updated Successfully',
+    product
+  });
+});
+
+export const deleteProduct = catchAsync(async (req, res, next) => {
+  await productService.deleteProduct(req);
 
   return res.status(200).json({
     status: 'success',
-    message: 'Category Updated Successfully',
-    result
+    message: 'Product Deleted Successfully'
   });
 });
