8056aab042d26506f94ff7b7c28922ef67fb4a40
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-12 22:57:14 +0200

    :fire: Add Update & Delete Routes

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,14 +3,27 @@ import { productController } from '../controllers/index';
 import auth from '../middlewares/auth';
 import { anyMulter } from '../utils/multer';
 
-const { getAllProducts, addProduct } = productController;
+const {
+  getAllProducts,
+  getProduct,
+  addProduct,
+  updateProductDetails,
+  updateProductMainImage,
+  updateProductImages,
+  deleteProduct
+} = productController;
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
+router.get('/:id', getProduct);
+router.patch('/:id/details', updateProductDetails);
+router.patch('/:id/main-image', anyMulter(), updateProductMainImage);
+router.patch('/:id/images', anyMulter(), updateProductImages);
+router.delete('/:id', deleteProduct);
 
 router.use(auth('admin', 'seller'));
 
-router.route('/').post(anyMulter(), addProduct);
+router.route('/').post(addProduct);
 
 export default router;
