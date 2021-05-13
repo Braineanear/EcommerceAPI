81a42af2e3df32c2e93f8aa188dba4c50a38accc
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-14 01:45:33 +0200

    :sparkles: Add Product Review Model

--- src/models/index.js
+++ src/models/index.js
@@ -2,5 +2,6 @@ import Token from './token.model';
 import User from './user.model';
 import Product from './product.model';
 import Category from './category.model';
+import Review from './review.model';
 
-export { Token, User, Product, Category };
+export { Token, User, Product, Category, Review };
