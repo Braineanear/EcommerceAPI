f4c78804770bd9fc81be4f78cede9be8f0fc6259
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-14 23:30:40 +0200

    :art: Code Format

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,32 +1,31 @@
 import passport from 'passport';
 import AppError from '../utils/appError';
 
-const verifyCallback = (req, resolve, reject, roles) => async (
-  err,
-  user,
-  info
-) => {
-  if (err || info || !user) {
-    return reject(new AppError('Please authenticate', 401));
-  }
-  req.user = user;
+const verifyCallback =
+  (req, resolve, reject, roles) => async (err, user, info) => {
+    if (err || info || !user) {
+      return reject(new AppError('Please authenticate', 401));
+    }
+    req.user = user;
 
-  if (!roles.includes(user.role)) {
-    return reject(new AppError('You do not have permission', 403));
-  }
+    if (!roles.includes(user.role)) {
+      return reject(new AppError('You do not have permission', 403));
+    }
 
-  resolve();
-};
+    resolve();
+  };
 
-const auth = (...roles) => async (req, res, next) =>
-  new Promise((resolve, reject) => {
-    passport.authenticate(
-      'jwt',
-      { session: false },
-      verifyCallback(req, resolve, reject, roles)
-    )(req, res, next);
-  })
-    .then(() => next())
-    .catch((err) => next(err));
+const auth =
+  (...roles) =>
+  async (req, res, next) =>
+    new Promise((resolve, reject) => {
+      passport.authenticate(
+        'jwt',
+        { session: false },
+        verifyCallback(req, resolve, reject, roles)
+      )(req, res, next);
+    })
+      .then(() => next())
+      .catch((err) => next(err));
 
 export default auth;
