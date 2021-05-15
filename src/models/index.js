c54dba7f956b65852b6a56157464ff81f895a712
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-15 20:26:52 +0200

    :sparkles: Add Seller Model

--- src/models/index.js
+++ src/models/index.js
@@ -4,5 +4,6 @@ import Product from './product.model';
 import Category from './category.model';
 import Review from './review.model';
 import Cart from './cart.model';
+import Seller from './seller.model';
 
-export { Token, User, Product, Category, Review, Cart };
+export { Token, User, Product, Category, Review, Cart, Seller };
