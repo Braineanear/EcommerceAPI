770dbb866921e3cb8fa4502cdadb314c2c28c213
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-15 20:28:29 +0200

    :sparkles: Add Login and Register For User & Seller

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -11,12 +11,11 @@ import { sendAfterResetPasswordMessage } from './email.service';
 import { verifyToken, generateAuthTokens } from './token.service';
 
 // Models
-import { Token, User } from '../models/index';
+import { Token, User, Seller } from '../models/index';
 
 /**
- * Login with username and password
- * @param {string} email
- * @param {string} password
+ * Login As User with Email & Password
+ * @param {Object} body
  * @returns {Promise<User>}
  */
 export const loginUserWithEmailAndPassword = catchAsync(async (body) => {
@@ -39,6 +38,33 @@ export const loginUserWithEmailAndPassword = catchAsync(async (body) => {
 
   // 5) If Everything OK, Send User
   return user;
+});
+
+/**
+ * Login As Seller with Email & Password
+ * @param {Object} body
+ * @returns {Promise<Seller>}
+ */
+export const loginSellerWithEmailAndPassword = catchAsync(async (body) => {
+  const { email, password } = body;
+
+  // 1) Check Email & Password
+  if (!email || !password) {
+    throw new AppError('All fields are required', 400);
+  }
+  // 2) Get Seller With Specific Email and Password
+  const seller = await Seller.findOne({ email }).select('+password');
+
+  // 3) Check if Passwords are The Same
+  const isMatch = await seller.isPasswordMatch(password);
+
+  // 4) If Email or Passwords isn't Correct
+  if (!seller || !isMatch) {
+    throw new AppError('Incorrect email or password', 403);
+  }
+
+  // 5) If Everything OK, Send Seller
+  return seller;
 });
 
 /**
@@ -95,22 +121,50 @@ export const resetPassword = catchAsync(
       tokenTypes.RESET_PASSWORD
     );
 
-    // 2) Find User and Update it's Password
-    const user = await User.findByIdAndUpdate(resetPasswordTokenDoc.user, {
-      password: newPassword,
-      passwordConfirmation: newPasswordConfirmation
-    });
+    if (resetPasswordTokenDoc.user.role === 'seller') {
+      // 2) Find User and Update it's Password
+      const user = await User.findByIdAndUpdate(resetPasswordTokenDoc.user, {
+        password: newPassword,
+        passwordConfirmation: newPasswordConfirmation
+      });
 
-    // 3) Check if User Already Exist
-    if (!user) {
-      throw new AppError('No User Found', 404);
+      // 3) Check if User Already Exist
+      if (!user) {
+        throw new AppError('No User Found', 404);
+      }
+
+      // 4) Sending After Reset Password Mail
+      await sendAfterResetPasswordMessage(user.email);
+
+      // 5) Deleteing User Reset Token
+      await Token.deleteMany({
+        user: user.id,
+        type: tokenTypes.RESET_PASSWORD
+      });
+    } else {
+      // 2) Find Seller and Update it's Password
+      const seller = await Seller.findByIdAndUpdate(
+        resetPasswordTokenDoc.user,
+        {
+          password: newPassword,
+          passwordConfirmation: newPasswordConfirmation
+        }
+      );
+
+      // 3) Check if Seller Doesn't Exist
+      if (!seller) {
+        throw new AppError('Seller Not Found', 404);
+      }
+
+      // 4) Sending After Reset Password Mail
+      await sendAfterResetPasswordMessage(seller.email);
+
+      // 5) Deleteing Seller Reset Token
+      await Token.deleteMany({
+        user: seller.id,
+        type: tokenTypes.RESET_PASSWORD
+      });
     }
-
-    // 4) Sending After Reset Password Mail
-    await sendAfterResetPasswordMessage(user.email);
-
-    // 5) Deleteing User Reset Token
-    await Token.deleteMany({ user: user.id, type: tokenTypes.RESET_PASSWORD });
   }
 );
 
@@ -126,17 +180,33 @@ export const verifyEmail = catchAsync(async (verifyEmailToken) => {
     tokenTypes.VERIFY_EMAIL
   );
 
-  // 2) Find User
-  const user = await User.findById(verifyEmailTokenDoc.user);
+  if (verifyEmailTokenDoc.user.role === 'user') {
+    // 2) Find User
+    const user = await User.findById(verifyEmailTokenDoc.user);
 
-  // 3) Check if User Already Exist
-  if (!user) {
-    throw new AppError('No User Found', 404);
+    // 3) Check if User Already Exist
+    if (!user) {
+      throw new AppError('No User Found', 404);
+    }
+
+    // 4) Deleting User Verify Email Token
+    await Token.deleteMany({ user: user.id, type: tokenTypes.VERIFY_EMAIL });
+
+    // 5) Update User isEmailVerified Filed (Set True)
+    await updateUserById(user.id, { isEmailVerified: true });
+  } else {
+    // 2) Find Seller
+    const seller = await Seller.findById(verifyEmailTokenDoc.user);
+
+    // 3) Check if Seller Doesn't Exist
+    if (!seller) {
+      throw new AppError('Seller Not Found', 404);
+    }
+
+    // 4) Deleting Seller Verify Email Token
+    await Token.deleteMany({ user: seller.id, type: tokenTypes.VERIFY_EMAIL });
+
+    // 5) Update Seller isEmailVerified Filed (Set True)
+    await updateUserById(seller.id, { isEmailVerified: true });
   }
-
-  // 4) Deleting User Verify Email Token
-  await Token.deleteMany({ user: user.id, type: tokenTypes.VERIFY_EMAIL });
-
-  // 5) Update User isEmailVerified Filed (Set True)
-  await updateUserById(user.id, { isEmailVerified: true });
 });
