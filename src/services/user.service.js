83bb39221ef5d80567ad6adc9813e7b813c25dc6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-15 23:08:54 +0200

    :sparkles: Update User Service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,21 +2,51 @@
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
 import APIFeatures from '../utils/apiFeatures';
+import dataUri from '../utils/datauri';
+import { uploadFile, destroyFile } from '../utils/cloudinary';
 
 // Models
 import { User } from '../models/index';
 
 /**
- * Create new user
- * @param {Object} req
- * @returns {Promise<User>}
+ * Create New User
+ * @param     {Object} body
+ * @param     {Object} profileImage
+ * @returns   {Promise<User>}
  */
-export const createUser = catchAsync(async (req) => {
-  const { name, username, email, password, passwordConfirmation } = req.body;
+export const createUser = catchAsync(async (body, profileImage) => {
+  const { name, username, email, password, passwordConfirmation, role } = body;
+  let address, companyName, phone;
+  if (body.companyName === '') {
+    companyName = '';
+  } else {
+    companyName = body.companyName;
+  }
 
-  if (!name || !username || !email || !password || !passwordConfirmation) {
+  if (body.address === '') {
+    address = '';
+  } else {
+    address = body.address;
+  }
+
+  if (body.phone === '') {
+    phone = '';
+  } else {
+    phone = body.phone;
+  }
+
+  if (
+    !name ||
+    !username ||
+    !email ||
+    !password ||
+    !passwordConfirmation ||
+    !role ||
+    profileImage.length === 0
+  ) {
     throw new AppError('All fields are required', 400);
   }
+
   // 1) Check if The Email Already Taken
   const isEmailTaken = await User.isEmailTaken(email);
 
@@ -25,13 +55,29 @@ export const createUser = catchAsync(async (req) => {
     throw new AppError('Email already taken', 400);
   }
 
+  // 3) Specifiy Folder Name Where The Images Are Going To Be Uploaded In Cloudinary
+  const folderName = `Users/${name.trim().split(' ').join('')}`;
+
+  // 4) Upload Image to Cloudinary
+  const image = await uploadFile(
+    dataUri(profileImage).content,
+    folderName,
+    600
+  );
+
   // 3) Create New User
   const user = await User.create({
     name,
     username,
     email,
     password,
-    passwordConfirmation
+    passwordConfirmation,
+    role,
+    companyName,
+    address,
+    phone,
+    profileImage: image.secure_url,
+    profileImageId: image.public_id
   });
 
   // 4) If Everything is OK, Send User Data
@@ -39,13 +85,9 @@ export const createUser = catchAsync(async (req) => {
 });
 
 /**
- * Query for users
- * @param {Object} filter - Mongo filter
- * @param {Object} options - Query options
- * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
- * @param {number} [options.limit] - Maximum number of results per page (default = 10)
- * @param {number} [options.page] - Current page (default = 1)
- * @returns {Promise<QueryResult>}
+ * Query Users
+ * @param     {Object} req - Request
+ * @returns   {Promise<Users>}
  */
 export const queryUsers = catchAsync(async (req) => {
   // 1) Get All Users
@@ -53,7 +95,7 @@ export const queryUsers = catchAsync(async (req) => {
 
   // 2) Check if No Users
   if (users.length === 0) {
-    throw new AppError('No Users Found', 404);
+    throw new AppError('Users Not Found', 404);
   }
 
   // 3) If Everything is OK, Send Users Data
@@ -61,46 +103,111 @@ export const queryUsers = catchAsync(async (req) => {
 });
 
 /**
- * Update user by id
- * @param {ObjectId} userId
- * @param {Object} updateBody
- * @returns {Promise<User>}
+ * Query User
+ * @param     {Object} id - User ID
+ * @return    {Promise<User>}
  */
-export const updateUserById = catchAsync(async (id, body) => {
-  // 1) Check if Email Already Taken
+export const queryUser = catchAsync(async (id) => {
+  // 1) Get User Using It's ID
+  const user = await User.findById(id);
+
+  // 2) Check If User Doesn't Exist
+  if (!user) {
+    throw new AppError(`No User Found With This ID: ${id}`, 404);
+  }
+
+  // 3) If Everything is OK, Send User Data;
+  return user;
+});
+
+/**
+ * Update User Details Using It's ID
+ * @param     {ObjectId}  id - User ID
+ * @param     {Object}    body - Updated Body
+ * @returns   {Promise<User>}
+ */
+export const updateUserDetails = catchAsync(async (id, body) => {
+  let user = await User.findById(id);
+
+  // 1) Check If User Doesn't Exist
+  if (!user) {
+    throw new AppError(`No User Found With This ID: ${id}`, 404);
+  }
+
+  // 2) Check if Email Taken Or Not
   const isEmailTaken = await User.isEmailTaken(body.email, id);
 
-  // 2) If Email Taken
   if (body.email && isEmailTaken) {
     throw new AppError('Email already taken', 400);
   }
 
   // 3) Find User Document and Update it
-  const user = await User.findByIdAndUpdate(id, body, {
+  user = await User.findByIdAndUpdate(id, body, {
     new: true,
     runValidators: true
   });
 
-  // 4) Check if User Already Exist
-  if (!user) {
-    throw new AppError('User not found', 404);
-  }
-
-  // 5) If Everything is OK, Send User Data
+  // 4) If Everything is OK, Send User Data
   return user;
 });
 
 /**
- * Delete user by id
- * @param {ObjectId} userId
- * @returns {Promise<User>}
+ * Update User Profile Image Using It's ID
+ * @param     {ObjectId}  id - User ID
+ * @param     {Object}    profileImage - Updated Profile Image
+ * @returns   {Promise<User>}
  */
-export const deleteUserById = catchAsync(async (userId) => {
+export const updateUserProfileImage = catchAsync(async (id, profileImage) => {
+  let user = await User.findById(id);
+
+  // 1) Check If User Doesn't Exist
+  if (!user) {
+    throw new AppError(`No User Found With This ID: ${id}`, 404);
+  }
+
+  // 2) Specifiy Folder Name Where The Profile Image Is Going To Be Uploaded In Cloudinary
+  const folderName = `Users/${user.name.trim().split(' ').join('')}`;
+
+  // 3) Destroy Image From Cloudinary
+  destroyFile(user.profileImageId);
+
+  // 4) Upload Image to Cloudinary
+  const image = await uploadFile(
+    dataUri(profileImage).content,
+    folderName,
+    600
+  );
+
+  // 5) Find User Document and Update it
+  user = await User.findByIdAndUpdate(
+    id,
+    {
+      profileImage: image.secure_url,
+      profileImageId: image.public_id
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  // 6) If Everything is OK, Send User Data
+  return user;
+});
+
+/**
+ * Delete Using It's ID
+ * @param     {ObjectId} id - User ID
+ */
+export const deleteUser = catchAsync(async (id) => {
   // 1) Find User Document and Delete it
-  const user = await User.findByIdAndDelete(userId);
+  const user = await User.findByIdAndDelete(id);
 
   // 2) Check if User Already Exist
   if (!user) {
-    throw new AppError('User not found', 404);
+    throw new AppError(`Not User Found With This ID: ${id}`, 404);
   }
+
+  // 3) Delete User Profile Image
+  destroyFile(user.profileImageId);
 });
