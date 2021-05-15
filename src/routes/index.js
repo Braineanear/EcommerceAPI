42fb55840c6249e3dfe97ec738e614e434d3f7b9
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-15 06:42:36 +0200

    :sparkles: Add Cart Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import userRoute from './user.route';
 import authRoute from './auth.route';
 import productRoute from './product.route';
 import categoryRoute from './category.route';
+import cartRoute from './cart.route';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/auth', authRoute);
 router.use('/user', userRoute);
 router.use('/product', productRoute);
 router.use('/category', categoryRoute);
+router.use('/cart', cartRoute);
 
 export default router;
