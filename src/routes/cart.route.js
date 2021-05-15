21058554e577d88302db62d495c639f474bf965b
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-15 06:42:27 +0200

    :sparkles: Add Cart Routes

---
+++ src/routes/cart.route.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import { cartController } from '../controllers/index';
+
+const {
+  addItemToCart,
+  subtractItemFromCart,
+  reduceByOne,
+  increaseByOne,
+  getCart,
+  deleteCart,
+  deleteItem
+} = cartController;
+
+const router = express.Router();
+
+router.post('/', addItemToCart);
+router.patch('/', subtractItemFromCart);
+router.get('/', getCart);
+router.delete('/', deleteCart);
+router.patch('/reduce-one', reduceByOne);
+router.patch('/increase-one', increaseByOne);
+router.delete('/:productId', deleteItem);
+export default router;
