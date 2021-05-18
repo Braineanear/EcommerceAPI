6907109299a5b7a580db56987db173862eb106e8
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-18 22:48:15 +0200

    :sparkles: Add Order Model

--- src/models/index.js
+++ src/models/index.js
@@ -4,5 +4,6 @@ import Product from './product.model';
 import Category from './category.model';
 import Review from './review.model';
 import Cart from './cart.model';
+import Order from './order.model';
 
-export { Token, User, Product, Category, Review, Cart };
+export { Token, User, Product, Category, Review, Cart, Order };
