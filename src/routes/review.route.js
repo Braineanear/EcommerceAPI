c1d579451941a3c1d10810d7b70a85c927c388e6
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-19 01:45:39 +0200

    :fire: Update Review Route

--- src/routes/review.route.js
+++ src/routes/review.route.js
@@ -8,16 +8,16 @@ import { reviewController } from '../controllers/index';
 
 const { getAllReviews, getReview, addReview, updateReview, deleteReview } =
   reviewController;
+
 const router = express.Router({ mergeParams: true });
 
 router.get('/', getAllReviews);
 router.get('/:reviewId', getReview);
 
-router.use(auth('user', 'admin'));
+router.use(auth('user', 'admin', 'seller'));
 
 router.post('/', addReview);
 
-router.patch('/:reviewId', updateReview);
-router.delete('/:reviewId', deleteReview);
+router.route('/:reviewId').patch(updateReview).delete(deleteReview);
 
 export default router;
