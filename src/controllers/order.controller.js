1778570c3a3cf7fe42dd286419839af2994d6ac3
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-19 01:43:38 +0200

    :fire: Update Order Controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -4,12 +4,20 @@ import catchAsync from '../utils/catchAsync';
 // Services
 import { orderService } from '../services/index';
 
+/**
+ * Create New Order
+ * @param   {Object} req
+ * @param   {Object} res
+ * @return  {JSON}
+ */
 export const createOrder = catchAsync(async (req, res) => {
+  // 1) Create New Order
   const { type, message, statusCode, order } = await orderService.createOrder(
     req.body,
     req.user
   );
 
+  // 2) Check If There is an Error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -17,6 +25,7 @@ export const createOrder = catchAsync(async (req, res) => {
     });
   }
 
+  // 3) If Everything is OK, Send Order
   return res.status(statusCode).json({
     type,
     message,
@@ -24,11 +33,19 @@ export const createOrder = catchAsync(async (req, res) => {
   });
 });
 
+/**
+ * Get All Orders
+ * @param   {Object} req
+ * @param   {Object} res
+ * @return  {JSON}
+ */
 export const getAllOrders = catchAsync(async (req, res) => {
+  // 1) Get All Orders
   const { type, message, statusCode, orders } = await orderService.queryOrders(
     req
   );
 
+  // 2) Check If There is an Error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -36,6 +53,7 @@ export const getAllOrders = catchAsync(async (req, res) => {
     });
   }
 
+  // 3) If Everything is OK, Send Orders
   return res.status(statusCode).json({
     type,
     message,
@@ -43,11 +61,19 @@ export const getAllOrders = catchAsync(async (req, res) => {
   });
 });
 
+/**
+ * Get Order Using It's ID
+ * @param   {Object} req
+ * @param   {Object} res
+ * @return  {JSON}
+ */
 export const getOrder = catchAsync(async (req, res) => {
+  // 1) Get Order Using It's ID
   const { type, message, statusCode, order } = await orderService.queryOrder(
     req.params.id
   );
 
+  // 2) Check If There is an Error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -55,6 +81,7 @@ export const getOrder = catchAsync(async (req, res) => {
     });
   }
 
+  // 3) If Everything is OK, Send Order
   return res.status(statusCode).json({
     type,
     message,
@@ -62,11 +89,19 @@ export const getOrder = catchAsync(async (req, res) => {
   });
 });
 
+/**
+ * Cancel Order
+ * @param   {Object} req
+ * @param   {Object} res
+ * @return  {JSON}
+ */
 export const cancelOrder = catchAsync(async (req, res) => {
+  // 1) Cancel Order Using It's ID
   const { type, message, statusCode } = await orderService.cancelOrder(
     req.params.id
   );
 
+  // 2) Check If There is an Error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -74,6 +109,7 @@ export const cancelOrder = catchAsync(async (req, res) => {
     });
   }
 
+  // 3) If Everything is OK, Send Message
   return res.status(statusCode).json({
     type,
     message
