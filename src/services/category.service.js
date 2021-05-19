4a1337e5d5a0732f0db88b3c08c21aae5bd83b32
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-19 03:55:16 +0200

    :fire: Update Category Service

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -10,14 +10,14 @@ import { Category } from '../models/index';
 /**
  * Create New Category
  * @param   {Object} body
+ * @param   {Object} file
  * @returns {Object<type|message|statusCode|category>}
  */
-export const createCategory = catchAsync(async (req) => {
-  const { name, description, status } = req.body;
-  let image = req.file;
+export const createCategory = catchAsync(async (body, file) => {
+  const { name, description } = body;
 
   // 1) Check If The User Entered All Fields
-  if (!name || !description || !status || !image) {
+  if (!name || !description || !file) {
     return {
       type: 'Error',
       message: 'All Fields Are Required',
@@ -29,24 +29,23 @@ export const createCategory = catchAsync(async (req) => {
   const folderName = `Category/${name.trim().split(' ').join('')}`;
 
   // 3) Generate a Buffer instance from a Data URI string
-  image = dataUri(image);
+  let image = dataUri(file);
 
   // 4) Upload Image to Cloudinary
   image = await uploadFile(image.content, folderName, 600);
 
   // 5) Create Body
-  const body = {
+  const object = {
     name,
     description,
-    status,
     image: image.secure_url,
     imageId: image.public_id
   };
 
-  // 7) Create New Category & Save Data at Database
-  const category = await Category.create(body);
+  // 6) Create New Category & Save Data at Database
+  const category = await Category.create(object);
 
-  // 8) If Everything is OK, Send Data
+  // 7) If Everything is OK, Send Data
   return {
     type: 'Success',
     message: 'Category Created Successfully',
@@ -57,7 +56,7 @@ export const createCategory = catchAsync(async (req) => {
 
 /**
  * Query users
- * @param   {Object} request
+ * @param   {Object} req
  * @returns {Object<type|message|statusCode|categories>}
  */
 export const queryCategories = catchAsync(async (req) => {
@@ -82,38 +81,11 @@ export const queryCategories = catchAsync(async (req) => {
 });
 
 /**
- * Query Category Using It's Name
- * @param   {String} name
+ * Query Category Using It's Id
+ * @param   {String} id
  * @returns {Object<type|message|statusCode|category>}
  */
-export const getCategoryByName = catchAsync(async (name) => {
-  // 1) Get Category From Database
-  const category = await Category.findOne({ name });
-
-  // 2) Check If Category Already Exist
-  if (!category) {
-    return {
-      type: 'Error',
-      message: `No Category Found With This Name: ${name}`,
-      statusCode: 404
-    };
-  }
-
-  // 3) If Everything is OK, Send Category
-  return {
-    type: 'Success',
-    message: 'Category Found Successfully',
-    statusCode: 200,
-    category
-  };
-});
-
-/**
- * Query Category Using It's Id
- * @param {String} id
- * @returns {Promise<Category>}
- */
-export const getCategoryById = catchAsync(async (id) => {
+export const queryCategory = catchAsync(async (id) => {
   // 1) Get Category From Database
   const category = await Category.findById(id);
 
@@ -143,7 +115,7 @@ export const getCategoryById = catchAsync(async (id) => {
  */
 export const updateCategoryDetails = catchAsync(async (id, body) => {
   // 1) Find Category Document
-  const category = await Category.findById(id);
+  let category = await Category.findById(id);
 
   // 2) Check If Category Already Exist
   if (!category) {
@@ -155,7 +127,7 @@ export const updateCategoryDetails = catchAsync(async (id, body) => {
   }
 
   // 3) Update Category
-  const result = await Category.findByIdAndUpdate(id, body, {
+  category = await Category.findByIdAndUpdate(id, body, {
     new: true,
     runValidators: true
   });
@@ -165,18 +137,19 @@ export const updateCategoryDetails = catchAsync(async (id, body) => {
     type: 'Success',
     message: 'Category Details Updated Successfully',
     statusCode: 200,
-    result
+    category
   };
 });
 
 /**
  * Update Category Image
- * @param   {Object} image
+ * @param   {ObjectId}  id
+ * @param   {Object}    image
  * @returns {Object<type|message|statusCode|category>}
  */
 export const updateCategoryImage = catchAsync(async (id, image) => {
   // 1) Get Category Data
-  const category = await Category.findById(id);
+  let category = await Category.findById(id);
 
   // 2) Check If Category Already Exist
   if (!category) {
@@ -202,7 +175,7 @@ export const updateCategoryImage = catchAsync(async (id, image) => {
   };
 
   // 6) Update Category
-  const result = await Category.findByIdAndUpdate(id, body, {
+  category = await Category.findByIdAndUpdate(id, body, {
     new: true,
     runValidators: true
   });
@@ -212,9 +185,10 @@ export const updateCategoryImage = catchAsync(async (id, image) => {
     type: 'Success',
     message: 'Category Image Updated Successfully',
     statusCode: 200,
-    result
+    category
   };
 });
+
 /**
  * Delete Category
  * @param   {ObjectId} id
