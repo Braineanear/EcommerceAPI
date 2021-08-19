cb73fe8ab9f945726c771b9f546cdd9fedb41807
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 00:05:22 +0200

    Add (restricted to) middleware

---
+++ src/middlewares/restrictedTo.js
@@ -0,0 +1,15 @@
+// Utils
+import AppError from '../utils/appError';
+
+const restrictedTo =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
+    }
+    next();
+  };
+
+export default restrictedTo;
