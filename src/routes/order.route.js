b0940811ace50ebaa0f6e8f678b10cbdf412109d
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 08:19:11 +0200

    Update order routes

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,17 +1,26 @@
+// Packages
 import express from 'express';
 
 // Controllers
 import { orderController } from '../controllers/index';
 
 // Middlewares
+import protect from '../middlewares/protect';
 
 const { createOrder, getAllOrders, getOrder, cancelOrder } = orderController;
 
+// Router Initialization
 const router = express.Router();
 
+// Protect All Routes
+router.use(protect);
 
-router.route('/').post(createOrder).get(getAllOrders);
+// Get All Orders Route
+// Create Order Route
+router.route('/').get(getAllOrders).post(createOrder);
 
+// Get Order Route
+// Cancel Order Route
 router.route('/:id').get(getOrder).delete(cancelOrder);
 
 export default router;
