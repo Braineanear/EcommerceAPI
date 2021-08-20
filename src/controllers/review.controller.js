898b089d2ec9bac58dcbbbc55397191bbee9b41d
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 07:14:29 +0200

    Remove logger

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,6 +1,3 @@
-// Config
-import logger from '../config/logger';
-
 // Utils
 import catchAsync from '../utils/catchAsync';
 
@@ -8,19 +5,19 @@ import catchAsync from '../utils/catchAsync';
 import { reviewService } from '../services/index';
 
 /**
- * Create New Review
- * @param       {Object}    req
- * @param       {Object}    res
- * @property    {ObjectId}  req.params.id
- * @property    {ObjectId}  req.user.id
- * @returns     {JSON}
+ * Create New Review Controller
+ * @param       { Object }    req
+ * @param       { Object }    res
+ * @property    { ObjectId }  req.params.id
+ * @property    { ObjectId }  req.user.id
+ * @returns     { JSON }
  */
 export const addReview = catchAsync(async (req, res) => {
-  // 1) Create Review
+  // 1) Create new review
   const { type, message, statusCode, review } =
     await reviewService.createReview(req.params.id, req.user.id, req.body);
 
-  // 2) Check If There is an Error
+  // 2) Check if there is an error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -28,7 +25,7 @@ export const addReview = catchAsync(async (req, res) => {
     });
   }
 
-  // 3) If Everything is OK, Send Review
+  // 3) If everything is OK, send data
   return res.status(statusCode).json({
     type,
     message,
@@ -37,29 +34,29 @@ export const addReview = catchAsync(async (req, res) => {
 });
 
 /**
- * Get All Reviews
- * @param     {Object}  req
- * @param     {Object}  res
- * @property  {String}  req.query.sort
- * @property  {String}  req.query.select
- * @property  {Number}  req.query.page
- * @property  {Number}  req.query.limit
- * @return    {JSON}
+ * Get All Reviews Controller
+ * @param     { Object }  req
+ * @param     { Object }  res
+ * @property  { String }  req.query.sort
+ * @property  { String }  req.query.select
+ * @property  { Number }  req.query.page
+ * @property  { Number }  req.query.limit
+ * @return    { JSON }
  */
 export const getAllReviews = catchAsync(async (req, res) => {
   let { page, sort, limit, select } = req.query;
 
-  // 1) Setting Default Params
+  // 1) Setting default params
   if (!page) page = 1;
   if (!sort) sort = '';
   if (!limit) limit = 10;
   if (!select) select = '';
 
-  // 1) Get All Reviews
+  // 1) Get all reviews
   const { type, message, statusCode, reviews } =
     await reviewService.queryReviews(req);
 
-  // 2) Check If There is an Error
+  // 2) Check if there is an error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -67,7 +64,7 @@ export const getAllReviews = catchAsync(async (req, res) => {
     });
   }
 
-  // 3) If Everything is OK, Send Data
+  // 3) If everything is OK, send data
   return res.status(statusCode).json({
     type,
     message,
@@ -76,20 +73,20 @@ export const getAllReviews = catchAsync(async (req, res) => {
 });
 
 /**
- * Get Review Using It's ID
- * @param     {Object}    req
- * @param     {Object}    res
- * @property  {ObjectId}  req.params.reviewId
- * @return    {JSON}
+ * Get Review Using It's ID Controller
+ * @param     { Object }    req
+ * @param     { Object }    res
+ * @property  { ObjectId }  req.params.reviewId
+ * @return    { JSON }
  */
 export const getReview = catchAsync(async (req, res) => {
   const { reviewId } = req.params;
 
-  // 1) Get Review Using It's ID
+  // 1) Get review using it's ID
   const { type, message, statusCode, review } =
     await reviewService.queryReviewById(reviewId);
 
-  // 2) Check If There is an Error
+  // 2) Check if there is an error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -97,7 +94,7 @@ export const getReview = catchAsync(async (req, res) => {
     });
   }
 
-  // 3) If Everything is OK, Send Review
+  // 3) If everything is OK, send data
   return res.status(statusCode).json({
     type,
     message,
@@ -106,18 +103,18 @@ export const getReview = catchAsync(async (req, res) => {
 });
 
 /**
- * Update Review
- * @param     {Object}    req
- * @param     {Object}    res
- * @property  {ObjectId}  req.params.reviewId
- * @return    {JSON}
+ * Update Review Controller
+ * @param     { Object }    req
+ * @param     { Object }    res
+ * @property  { ObjectId }  req.params.reviewId
+ * @return    { JSON }
  */
 export const updateReview = catchAsync(async (req, res) => {
-  // 1) Update Review Using It's ID
+  // 1) Update review using it's ID
   const { type, message, statusCode, review } =
     await reviewService.updateReview(req.params.reviewId, req.body);
 
-  // 2) Check If There is an Error
+  // 2) Check if there is an error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -125,7 +122,7 @@ export const updateReview = catchAsync(async (req, res) => {
     });
   }
 
-  // 3) If Everything is OK, Send Review
+  // 3) If everything is OK, send data
   return res.status(statusCode).json({
     type,
     message,
@@ -134,19 +131,19 @@ export const updateReview = catchAsync(async (req, res) => {
 });
 
 /**
- * Delete Review
- * @param   {Object}    req
- * @param   {Object}    res
- * @param   {ObjectId}  req.params.reviewId
- * @return  {JSON}
+ * Delete Review Controller
+ * @param   { Object }    req
+ * @param   { Object }    res
+ * @param   { ObjectId }  req.params.reviewId
+ * @return  { JSON }
  */
 export const deleteReview = catchAsync(async (req, res) => {
-  // 1) Delete Review
+  // 1) Delete review using it's ID
   const { type, message, statusCode } = await reviewService.deleteReview(
     req.params.reviewId
   );
 
-  // 2) Check If There is an Error
+  // 2) Check if there is an error
   if (type === 'Error') {
     return res.status(statusCode).json({
       type,
@@ -154,7 +151,7 @@ export const deleteReview = catchAsync(async (req, res) => {
     });
   }
 
-  // 3) If Everything Is OK, Send Message
+  // 3) If everything is OK, send data
   return res.status(statusCode).json({
     type,
     message
