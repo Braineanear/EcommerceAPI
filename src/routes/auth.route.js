7c5e4e9edea1b98dd441e4ca4939e570bc45576f
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 08:13:09 +0200

    Update auth routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,10 +4,45 @@ import express from 'express';
 // Controllers
 import { authController } from '../controllers/index';
 
-const { token } = authController;
+// Utils
+import { singleFile } from '../utils/multer';
 
+const {
+  signin,
+  signup,
+  logout,
+  refreshTokens,
+  forgotPassword,
+  resetPassword,
+  verifyEmail,
+  sendVerificationEmail
+} = authController;
+
+// Router Initialization
 const router = express.Router();
 
-router.post('/token', token);
+// Login Route
+router.post('/login', signin);
+
+// Register Route (Multer Middleware)
+router.post('/register', singleFile('image'), signup);
+
+// Logout Route
+router.post('/logout', logout);
+
+// Refresh Token Route
+router.post('/refresh-tokens', refreshTokens);
+
+// Forgot Password Route
+router.post('/forgot-password', forgotPassword);
+
+// Reset Password Route
+router.post('/reset-password', resetPassword);
+
+// Send Verification Email Route
+router.post('/send-verification-email', sendVerificationEmail);
+
+// Verify Email Route
+router.post('/verify-email', verifyEmail);
 
 export default router;
