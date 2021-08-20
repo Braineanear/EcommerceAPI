2466a84f814ca8ad47c3c0efed6ee1b44cf261f7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 08:21:35 +0200

    Update product routes

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,11 @@
+// Packages
 import express from 'express';
 
 // Controllers
 import { productController } from '../controllers/index';
 
 // Middlewares
+import protect from '../middlewares/protect';
 
 // Utils
 import { anyMulter } from '../utils/multer';
@@ -23,19 +25,39 @@ const {
   productStats
 } = productController;
 
+// Router Initialization
 const router = express.Router();
 
 router.use('/:id/reviews', reviewRoute);
 
+// Get ALl Products Route
 router.get('/', getAllProducts);
+
+// Get Product Route
 router.get('/:id', getProduct);
+
+// Get Top 5 Cheapeast Products Route
 router.get('/top-5-cheap', top5Cheap, getAllProducts);
+
+// Get Product Stats Route
 router.get('/product-stats', productStats);
 
+// Protect All Next Routes
+router.use(protect);
+
+// Add Product (Multer Middleware) Route
 router.post('/', anyMulter(), addProduct);
+
+// Update Product Details Route
 router.patch('/:id/details', updateProductDetails);
+
+// Update Product Main Image (Multer Middleware) Route
 router.patch('/:id/main-image', anyMulter(), updateProductMainImage);
+
+// Update Product Images (Multer Middleware) Route
 router.patch('/:id/images', anyMulter(), updateProductImages);
+
+// Delete Product Route
 router.delete('/:id', deleteProduct);
 
 export default router;
