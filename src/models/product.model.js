1334ea3be5193b528baacf588f870cddeecbb1a4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-20 14:49:38 +0200

    Remove product name and description length

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -10,15 +10,7 @@ const productSchema = mongoose.Schema(
       type: String,
       required: [true, 'A product must have a name'],
       unique: true,
-      trim: true,
-      maxlength: [
-        40,
-        'A product name must have less or equal then 40 characters'
-      ],
-      minlength: [
-        10,
-        'A product name must have more or equal then 10 characters'
-      ]
+      trim: true
     },
     slug: String,
     mainImage: {
@@ -37,15 +29,7 @@ const productSchema = mongoose.Schema(
     },
     description: {
       type: String,
-      required: [true, 'A product must have a description'],
-      min: [
-        10,
-        'A product description must have more or equal than 10 characters'
-      ],
-      max: [
-        100,
-        'A product description must have less or equal than 100 characters'
-      ]
+      required: [true, 'A product must have a description']
     },
     category: {
       type: mongoose.Schema.ObjectId,
