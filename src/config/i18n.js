ca4fc1fae82ad064bac6ba5891eb21ed57f6d549
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-08-29 00:06:13 +0200

    Add i18n configs

---
+++ src/config/i18n.js
@@ -0,0 +1,22 @@
+export const availableLangs = ['ar', 'en'];
+
+export const messages = {
+  en: {
+    profileImageRequired: 'Profile Image Is Required, Please Upload an Image',
+    fieldsRequired: 'All Fields Are Required',
+    passwordLength:
+      'Password must be longer than 8 characters and contains letters, numbers, and symbols',
+    roleRestriction: 'Role must be one of the following: user or seller',
+    emailTaken: 'Email Is Already Taken',
+    successfullSignUp: 'Account created successfull, please verify your email!'
+  },
+  ar: {
+    profileImageRequired: 'الصورة الشخصية مطلوبة. رجاءً قم برفع صورة',
+    fieldsRequired: 'برجاء إدخال جميع البيانات',
+    passwordLength:
+      'كلمة المرور لا يجب أن تقل عن 8 أحرف ومكونة من أحرف وأرقام ورموز',
+    roleRestriction: 'نوع الحساب يجب أن يكون واحد من هؤلاء: مستخدم أو بائع',
+    emailTaken: 'البريد الإلكتروني مستخدم من قبل',
+    successfullSignUp: 'تم إنشاء الحساب بنجاح. برجاء تفعيل البريد الإلكتروني'
+  }
+};
