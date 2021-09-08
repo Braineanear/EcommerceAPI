2600ff1d2c115e6ce4288fa2c4dcaf895e438bf4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-08 17:27:54 +0200

    Fix a bug in queryReviews(select only reviews of the product) and make a check on createReview to check if the user make a review before on the same product

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -22,7 +22,18 @@ export const createReview = catchAsync(async (product, user, body) => {
     };
   }
 
-  // 2) Create Review
+  const checkUser = await Review.find({ user, product });
+
+  // 2) Check if the user make a review before on that product
+  if (checkUser.length !== 0) {
+    return {
+      type: 'Error',
+      message: 'onlyOneReview',
+      statusCode: 400
+    };
+  }
+
+  // 3) Create Review
   const newReview = await Review.create({
     product,
     user,
@@ -30,7 +41,7 @@ export const createReview = catchAsync(async (product, user, body) => {
     rating
   });
 
-  // 3) If Everything is OK, Send Review
+  // 4) If Everything is OK, Send Review
   return {
     type: 'Success',
     message: 'successfulReviewCreate',
@@ -46,9 +57,14 @@ export const createReview = catchAsync(async (product, user, body) => {
  */
 export const queryReviews = catchAsync(async (req) => {
   // 1) Get All Reviews
-  const reviews = await APIFeatures(req, Review);
+  let reviews = await APIFeatures(req, Review);
 
-  // 2) Check if Reviews Doesn't Exist
+  // 2) Filter review to select only reviews of the product only
+  reviews = reviews.filter(
+    (review) => review.product.toString() === req.params.id.toString()
+  );
+
+  // 3) Check if Reviews Doesn't Exist
   if (reviews.length === 0) {
     return {
       type: 'Error',
@@ -57,7 +73,7 @@ export const queryReviews = catchAsync(async (req) => {
     };
   }
 
-  // 3) If Everything is OK, Send Reviews
+  // 4) If Everything is OK, Send Reviews
   return {
     type: 'Success',
     message: 'successfulReviewsFound',
@@ -153,7 +169,8 @@ export const deleteReview = catchAsync(async (user, id) => {
     };
   }
 
-  if (user._id !== review.user) {
+  // 3) Check if the user is the creator of the review to delete it
+  if (user._id.toString() !== review.user.toString()) {
     return {
       type: 'Error',
       statusCode: 400,
@@ -161,10 +178,10 @@ export const deleteReview = catchAsync(async (user, id) => {
     };
   }
 
-  // 3) Delete Review
+  // 4) Delete Review
   await Review.findByIdAndDelete(id);
 
-  // 4) If Everything is OK, Send Message
+  // 5) If Everything is OK, Send Message
   return {
     type: 'Success',
     message: 'successfulReviewDelete',
