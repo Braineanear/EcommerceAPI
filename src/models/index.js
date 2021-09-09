ea1db067b7cb4020d710ad99a11589d343dc6029
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-09 23:30:03 +0200

    Update models index

--- src/models/index.js
+++ src/models/index.js
@@ -5,5 +5,17 @@ import Review from './review.model';
 import Cart from './cart.model';
 import Order from './order.model';
 import Token from './token.model';
+import Discount from './discount.model';
+import Favorite from './favorite.model';
 
-export { User, Product, Category, Review, Cart, Order, Token };
+export {
+  User,
+  Product,
+  Category,
+  Review,
+  Cart,
+  Order,
+  Token,
+  Discount,
+  Favorite
+};
