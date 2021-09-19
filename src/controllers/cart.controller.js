0c6ecbd493907552464fbcce0e902fac15423deb
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-19 13:48:19 +0200

    Add selectedColor and selectedSize fields

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -8,17 +8,25 @@ import { cartService } from '../services/index';
  * @desc      Add Product To Cart Controller
  * @param     { Object } req - Request object
  * @param     { Object } res - Response object
+ * @property  { String } req.user.email - User email
  * @property  { String } req.body.productId - Product ID
  * @property  { Number } req.body.quantity - Product quantity
- * @property  { Object } req.user - An object contains logged in user data
+ * @property  { String } req.body.selectedColor - Selected color
+ * @property  { String } req.body.selectedSize - Selected size
  * @returns   { JSON } - A JSON object representing the type, message, and the cart
  */
 export const addItemToCart = catchAsync(async (req, res) => {
-  const { productId, quantity } = req.body;
+  const { productId, quantity, selectedColor, selectedSize } = req.body;
 
   // 1) Add product to cart
   const { type, message, statusCode, cart } =
-    await cartService.addProductToCart(req.user.email, productId, quantity);
+    await cartService.addProductToCart(
+      req.user.email,
+      productId,
+      quantity,
+      selectedColor,
+      selectedSize
+    );
 
   // 2) Check if there is an error
   if (type === 'Error') {
@@ -42,13 +50,19 @@ export const addItemToCart = catchAsync(async (req, res) => {
  * @param     { Object } res - Response object
  * @property  { Object } req.user - An object contains logged in user data
  * @property  { String } req.body.productId - Product ID
+ * @property  { String } req.body.selectedColor - Selected color
+ * @property  { String } req.body.selectedSize - Selected size
  * @returns   { JSON } - A JSON object representing the type, message, and the cart
  */
 export const reduceByOne = catchAsync(async (req, res) => {
+  const { productId, selectedColor, selectedSize } = req.body;
+
   // 1) Reduce product quantity by one from cart
   const { type, message, statusCode, cart } = await cartService.reduceByOne(
     req.user.email,
-    req.body.productId
+    productId,
+    selectedColor,
+    selectedSize
   );
 
   // 2) Check if there is an error
@@ -71,15 +85,21 @@ export const reduceByOne = catchAsync(async (req, res) => {
  * @desc      Increase Product By One Controller
  * @param     { Object } req - Request object
  * @param     { Object } res - Response object
- * @property  { String } req.body.productId - Product ID
  * @property  { Object } req.user - An object contains logged in user data
+ * @property  { String } req.body.productId - Product ID
+ * @property  { String } req.body.selectedColor - Selected color
+ * @property  { String } req.body.selectedSize - Selected size
  * @returns   { JSON } - A JSON object representing the type, message, and the cart
  */
 export const increaseByOne = catchAsync(async (req, res) => {
+  const { productId, selectedColor, selectedSize } = req.body;
+
   // 1) Increase product by one
   const { type, message, statusCode, cart } = await cartService.increaseByOne(
     req.user.email,
-    req.body.productId
+    productId,
+    selectedColor,
+    selectedSize
   );
 
   // 2) Check if there is an error
@@ -159,15 +179,21 @@ export const deleteCart = catchAsync(async (req, res) => {
  * @desc      Delete Product From Cart Controller
  * @param     { Object } req - Request object
  * @param     { Object } res - Response object
- * @property  { String } req.params.productId - Product ID
  * @property  { String } req.user.email - User email address
+ * @property  { String } req.params.productId - Product ID
+ * @property  { String } req.body.selectedColor - Selected color
+ * @property  { String } req.body.selectedSize - Selected size
  * @returns   { JSON } - A JSON object representing the type, message and the cart
  */
 export const deleteItem = catchAsync(async (req, res) => {
+  const { selectedColor, selectedSize } = req.body;
+
   // 1) Delete product from cart
   const { type, message, statusCode, cart } = await cartService.deleteItem(
     req.user.email,
-    req.params.productId
+    req.params.productId,
+    selectedColor,
+    selectedSize
   );
 
   // 2) Check if there is an error
