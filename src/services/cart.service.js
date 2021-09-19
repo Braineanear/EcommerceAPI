702b705bfbc7e5cad8436196bb078fe94a5c205c
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-19 13:48:29 +0200

    Add selectedColor and selectedSize fields

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -9,10 +9,12 @@ import { Cart, Product } from '../models/index';
  * @param   { String } email - User email address
  * @param   { String } productId - Product ID
  * @param   { Number } quantity - Product quantity
+ * @param   { String } selectedColor - Selected Color
+ * @param   { String } selectedSize - Selected Size
  * @returns { Object<type|message|statusCode|cart> }
  */
 export const addProductToCart = catchAsync(
-  async (email, productId, quantity) => {
+  async (email, productId, quantity, selectedColor, selectedSize) => {
     const cart = await Cart.findOne({ email });
     const product = await Product.findById(productId);
 
@@ -37,8 +39,14 @@ export const addProductToCart = catchAsync(
       // Check product index
       if (indexFound !== -1 && quantity <= 0) {
         cart.items.splice(indexFound, 1);
-      } else if (indexFound !== -1) {
-        // In case product exist in the cart
+      } else if (
+        indexFound !== -1 &&
+        cart.items[indexFound].selectedColor.toString() ===
+          selectedColor.toString() &&
+        cart.items[indexFound].selectedSize.toString() ===
+          selectedSize.toString()
+      ) {
+        // In case product exist in the cart and have the same color and size.
         cart.items[indexFound].totalProductQuantity += quantity;
         cart.items[indexFound].totalProductPrice += price * quantity;
         cart.totalQuantity += quantity;
@@ -49,6 +57,8 @@ export const addProductToCart = catchAsync(
         // Update totalQuantity & totalPrice
         cart.items.push({
           product: productId,
+          selectedColor: selectedColor,
+          selectedSize: selectedSize,
           totalProductQuantity: quantity,
           totalProductPrice: price * quantity
         });
@@ -81,6 +91,8 @@ export const addProductToCart = catchAsync(
       items: [
         {
           product: productId,
+          selectedColor: selectedColor,
+          selectedSize: selectedSize,
           totalProductQuantity: quantity,
           totalProductPrice: price * quantity
         }
@@ -106,145 +118,179 @@ export const addProductToCart = catchAsync(
  * @desc    Reduce Product Quantity By One
  * @param   { String } email - User email address
  * @param   { String } productId - Product ID
+ * @param   { String } selectedColor - Selected Color
+ * @param   { String } selectedSize - Selected Size
  * @returns { Object<type|message|statusCode|cart> }
  */
-export const reduceByOne = catchAsync(async (email, productId) => {
-  const cart = await Cart.findOne({ email });
-  const product = await Product.findById(productId);
-  const { price } = product;
+export const reduceByOne = catchAsync(
+  async (email, productId, selectedColor, selectedSize) => {
+    const cart = await Cart.findOne({ email });
+    const product = await Product.findById(productId);
+    const { price } = product;
 
-  // 1) Check if cart already exist
-  if (!cart) {
+    // 1) Check if cart already exist
+    if (!cart) {
+      return {
+        type: 'Error',
+        message: 'noCartForUser',
+        statusCode: 404
+      };
+    }
+
+    // 2) Find product index inside cart
+    const indexesFound = cart.items.reduce((a, e, i) => {
+      if (e.product.toString() === productId.toString()) a.push(i);
+      return a;
+    }, []);
+
+    // 3) Check if product doesn't exist in the cart
+    if (indexesFound === -1) {
+      return {
+        type: 'Error',
+        message: `noProductInCartWithID`,
+        statusCode: 404
+      };
+    }
+
+    // 4) Update cart & product data
+    for (const indexFound of indexesFound) {
+      if (
+        cart.items[indexFound].totalProductQuantity === 1 &&
+        cart.items[indexFound].selectedColor.toString() ===
+          selectedColor.toString() &&
+        cart.items[indexFound].selectedSize.toString() ===
+          selectedSize.toString()
+      ) {
+        cart.items.splice(indexFound, 1);
+        cart.totalQuantity -= 1;
+        cart.totalPrice -= price;
+      } else if (
+        cart.items[indexFound].selectedColor.toString() ===
+          selectedColor.toString() &&
+        cart.items[indexFound].selectedSize.toString() ===
+          selectedSize.toString()
+      ) {
+        const updatedProductTotalQuantity =
+          cart.items[indexFound].totalProductQuantity - 1;
+        const updatedProductTotalPrice =
+          cart.items[indexFound].totalProductPrice - price;
+        const updatedCartTotalQuantity = cart.totalQuantity - 1;
+        const updatedCartTotalPrice = cart.totalPrice - price;
+
+        cart.items[indexFound].totalProductQuantity =
+          updatedProductTotalQuantity;
+        cart.items[indexFound].totalProductPrice = updatedProductTotalPrice;
+        cart.totalQuantity = updatedCartTotalQuantity;
+        cart.totalPrice = updatedCartTotalPrice;
+      }
+    }
+
+    // 5) Save updated data
+    await cart.save();
+
+    // 6) Find cart using it's ID
+    const updatedCart = await Cart.findById(cart._id);
+
+    // 7) If everything is OK, send data
     return {
-      type: 'Error',
-      message: 'noCartForUser',
-      statusCode: 404
+      type: 'Success',
+      message: 'successfulReduceByOne',
+      statusCode: 200,
+      cart: updatedCart
     };
   }
-
-  // 2) Find product index inside cart
-  const indexFound = cart.items.findIndex(
-    (item) => item.product.toString() === productId.toString()
-  );
-
-  // 3) Check if product doesn't exist in the cart
-  if (indexFound === -1) {
-    return {
-      type: 'Error',
-      message: `noProductInCartWithID`,
-      statusCode: 404
-    };
-  }
-
-  // 4) Update cart & product data
-  if (cart.items[indexFound].totalProductQuantity === 1) {
-    cart.items.splice(indexFound, 1);
-    cart.totalQuantity -= 1;
-    cart.totalPrice -= price;
-  } else {
-    const updatedProductTotalQuantity =
-      cart.items[indexFound].totalProductQuantity - 1;
-    const updatedProductTotalPrice =
-      cart.items[indexFound].totalProductPrice - price;
-    const updatedCartTotalQuantity = cart.totalQuantity - 1;
-    const updatedCartTotalPrice = cart.totalPrice - price;
-
-    cart.items[indexFound].totalProductQuantity = updatedProductTotalQuantity;
-    cart.items[indexFound].totalProductPrice = updatedProductTotalPrice;
-    cart.totalQuantity = updatedCartTotalQuantity;
-    cart.totalPrice = updatedCartTotalPrice;
-  }
-
-  // 5) Save updated data
-  await cart.save();
-
-  // 6) Find cart using it's ID
-  const updatedCart = await Cart.findById(cart._id);
-
-  // 7) If everything is OK, send data
-  return {
-    type: 'Success',
-    message: 'successfulReduceByOne',
-    statusCode: 200,
-    cart: updatedCart
-  };
-});
+);
 
 /**
  * @desc    Increase Product Quantity By One
  * @param   { String } email - User email address
  * @param   { String } productId - Product ID
+ * @param   { String } selectedColor - Selected Color
+ * @param   { String } selectedSize - Selected Size
  * @returns { Object<type|message|statusCode|cart> }
  */
-export const increaseByOne = catchAsync(async (email, productId) => {
-  let cart = await Cart.findOne({ email });
-  const product = await Product.findById(productId);
+export const increaseByOne = catchAsync(
+  async (email, productId, selectedColor, selectedSize) => {
+    let cart = await Cart.findOne({ email });
+    const product = await Product.findById(productId);
 
-  // 1) Check if product doesn't exist
-  if (!product) {
+    // 1) Check if product doesn't exist
+    if (!product) {
+      return {
+        type: 'Error',
+        statusCode: 404,
+        message: 'noProductFound'
+      };
+    }
+
+    const { price } = product;
+
+    // 2) Check if cart doesn't exist
+    if (!cart) {
+      return {
+        type: 'Error',
+        message: 'noCartForUser',
+        statusCode: 404
+      };
+    }
+
+    // 3) Find products indexes inside cart
+    const indexesFound = cart.items.reduce((a, e, i) => {
+      if (e.product.toString() === productId.toString()) a.push(i);
+      return a;
+    }, []);
+
+    // 4) Check if product doesn't exist in the cart
+    if (indexesFound.length === 0) {
+      return {
+        type: 'Error',
+        message: 'noProductInCartWithID',
+        statusCode: 404
+      };
+    }
+
+    // 5) Update cart & product data
+    for (const indexFound of indexesFound) {
+      if (
+        cart.items[indexFound].selectedColor.toString() ===
+          selectedColor.toString() &&
+        cart.items[indexFound].selectedSize.toString() ===
+          selectedSize.toString()
+      ) {
+        const updatedProductTotalQuantity =
+          cart.items[indexFound].totalProductQuantity + 1;
+        const updatedProductTotalPrice =
+          cart.items[indexFound].totalProductPrice + price;
+        const updatedCartTotalQuantity = cart.totalQuantity + 1;
+        const updatedCartTotalPrice = cart.totalPrice + price;
+
+        if (updatedProductTotalQuantity <= 0 && updatedProductTotalPrice <= 0) {
+          cart.items.splice(indexFound, 1);
+        } else {
+          cart.items[indexFound].totalProductQuantity =
+            updatedProductTotalQuantity;
+          cart.items[indexFound].totalProductPrice = updatedProductTotalPrice;
+          cart.totalQuantity = updatedCartTotalQuantity;
+          cart.totalPrice = updatedCartTotalPrice;
+        }
+      }
+    }
+
+    // 6) Save updated data
+    await cart.save();
+
+    // 7) Find cart using it's ID
+    const updatedCart = await Cart.findById(cart._id);
+
+    // 8) If everything is OK, send data
     return {
-      type: 'Error',
-      statusCode: 404,
-      message: 'noProductFound'
+      type: 'Success',
+      message: 'successfulIncreaseByOne',
+      statusCode: 200,
+      cart: updatedCart
     };
   }
-
-  const { price } = product;
-
-  // 2) Check if cart doesn't exist
-  if (!cart) {
-    return {
-      type: 'Error',
-      message: 'noCartForUser',
-      statusCode: 404
-    };
-  }
-
-  // 3) Find product index inside cart
-  const indexFound = cart.items.findIndex(
-    (item) => item.product.toString() === productId.toString()
-  );
-
-  // 4) Check if product doesn't exist in the cart
-  if (indexFound === -1) {
-    return {
-      type: 'Error',
-      message: 'noProductInCartWithID',
-      statusCode: 404
-    };
-  }
-
-  // 5) Update cart & product data
-  const updatedProductTotalQuantity =
-    cart.items[indexFound].totalProductQuantity + 1;
-  const updatedProductTotalPrice =
-    cart.items[indexFound].totalProductPrice + price;
-  const updatedCartTotalQuantity = cart.totalQuantity + 1;
-  const updatedCartTotalPrice = cart.totalPrice + price;
-
-  if (updatedProductTotalQuantity <= 0 && updatedProductTotalPrice <= 0) {
-    cart.items.splice(indexFound, 1);
-  } else {
-    cart.items[indexFound].totalProductQuantity = updatedProductTotalQuantity;
-    cart.items[indexFound].totalProductPrice = updatedProductTotalPrice;
-    cart.totalQuantity = updatedCartTotalQuantity;
-    cart.totalPrice = updatedCartTotalPrice;
-  }
-
-  // 6) Save updated data
-  await cart.save();
-
-  // 7) Find cart using it's ID
-  const updatedCart = await Cart.findById(cart._id);
-
-  // 8) If everything is OK, send data
-  return {
-    type: 'Success',
-    message: 'successfulIncreaseByOne',
-    statusCode: 200,
-    cart: updatedCart
-  };
-});
+);
 
 /**
  * @desc    Get Cart
@@ -304,55 +350,79 @@ export const deleteCart = catchAsync(async (email) => {
  * @desc    Delete Cart Item
  * @param   { String } email - User email address
  * @param   { String } productId - Product ID
+ * @param   { String } selectedColor - Selected Color
+ * @param   { String } selectedSize - Selected Size
  * @returns { Object<type|message|statusCode|cart> }
  */
-export const deleteItem = catchAsync(async (email, productId) => {
-  let cart = await Cart.findOne({ email });
+export const deleteItem = catchAsync(
+  async (email, productId, selectedColor, selectedSize) => {
+    let cart = await Cart.findOne({ email });
 
-  // 1) Check if cart doesn't exist
-  if (!cart) {
+    // 1) Check if cart doesn't exist
+    if (!cart) {
+      return {
+        type: 'Error',
+        message: 'noCartForUser',
+        statusCode: 404
+      };
+    }
+
+    const product = await Product.findById(productId);
+
+    // 2) Check if product doesn't exist
+    if (!product) {
+      return {
+        type: 'Error',
+        message: 'noProductInCartWithID',
+        statusCode: 404
+      };
+    }
+
+    const indexesFound = cart.items.reduce((a, e, i) => {
+      if (e.product.toString() === productId.toString()) a.push(i);
+      return a;
+    }, []);
+
+    for (const indexFound of indexesFound) {
+      if (
+        cart.items[indexFound].selectedColor.toString() ===
+          selectedColor.toString() &&
+        cart.items[indexFound].selectedSize.toString() ===
+          selectedSize.toString()
+      ) {
+        const totalPrice =
+          cart.totalPrice - cart.items[indexFound].totalProductPrice;
+        const totalQuantity =
+          cart.totalQuantity - cart.items[indexFound].totalProductQuantity;
+
+        // await cart.children.id(itemId).remove();
+
+        // 3) Update cart by deleting product
+        // eslint-disable-next-line no-await-in-loop
+        cart = await Cart.findOneAndUpdate(
+          { email },
+          {
+            $pull: {
+              items: {
+                product: productId,
+                selectedColor: selectedColor,
+                selectedSize: selectedSize
+              }
+            },
+            totalPrice,
+            totalQuantity
+          },
+          { new: true }
+        );
+      }
+    }
+
+    // 4) If everything is OK, send data
     return {
-      type: 'Error',
-      message: 'noCartForUser',
-      statusCode: 404
+      type: 'Success',
+      message: 'successfulDeleteItemFromCart',
+      statusCode: 200,
+      cart
     };
   }
-
-  const product = await Product.findById(productId);
-
-  // 2) Check if product doesn't exist
-  if (!product) {
-    return {
-      type: 'Error',
-      message: 'noProductInCartWithID',
-      statusCode: 404
-    };
-  }
-
-  const indexFound = cart.items.findIndex(
-    (item) => item.product.toString() === productId.toString()
-  );
-
-  const totalPrice = cart.totalPrice - cart.items[indexFound].totalProductPrice;
-  const totalQuantity =
-    cart.totalQuantity - cart.items[indexFound].totalProductQuantity;
-
-  // 3) Update cart by deleting product
-  cart = await Cart.findOneAndUpdate(
-    { email },
-    {
-      $pull: { items: { product: productId } },
-      totalPrice,
-      totalQuantity
-    },
-    { new: true }
-  );
-
-  // 4) If everything is OK, send data
-  return {
-    type: 'Success',
-    message: 'successfulDeleteItemFromCart',
-    statusCode: 200,
-    cart
-  };
-});
+);
