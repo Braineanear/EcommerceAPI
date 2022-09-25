430c233bcd6000ab59d2a7846f0651fcd2e49137
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:32:43 +0200

    refactor(product): use base service and repository

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -3,24 +3,23 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { FilterQuery, UpdateQuery, Types } from 'mongoose';
+import { Types } from 'mongoose';
 import { DebuggerService } from '@shared/debugger/debugger.service';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
+import { BaseService } from '@shared/services/base.service';
 import { AwsS3Service } from '@shared/aws/aws.service';
 import { IAwsS3Response } from '@shared/aws/interfaces/aws.interface';
 import { ImageService } from '@modules/image/image.service';
 import { CategoryService } from '@modules/category/category.service';
 import { ColorService } from '@modules/color/color.service';
 import { BrandService } from '@modules/brand/brand.service';
+import { TagService } from '@modules/tag/tag.service';
 import { SizeService } from '@modules/size/size.service';
 import { ProductRepository } from './repositories/product.repository';
-import { FindProductsDto } from './dtos/find-products.dto';
 import { IProductDocument } from './interfaces/product.interface';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 import { CreateProductDto } from './dtos/create-product.dto';
 
 @Injectable()
-export class ProductService {
+export class ProductService extends BaseService<ProductRepository> {
   constructor(
     protected readonly repository: ProductRepository,
     protected readonly debuggerService: DebuggerService,
@@ -29,175 +28,69 @@ export class ProductService {
     protected readonly categoryService: CategoryService,
     protected readonly brandService: BrandService,
     protected readonly sizeService: SizeService,
+    protected readonly tagService: TagService,
     protected readonly colorService: ColorService,
-  ) {}
+  ) {
+    super();
+  }
 
   async create(createProductDto: CreateProductDto): Promise<IProductDocument> {
     const category = await this.categoryService.findOne({
-      name: createProductDto.category,
+      code: createProductDto.category,
     });
-
-    if (!category) {
-      this.debuggerService.error(
-        'Category not found',
-        'ProductService',
-        'create',
-      );
-
-      throw new NotFoundException('Category not found');
-    }
 
     createProductDto.category = category._id;
 
-    const brand = await this.brandService.findOne({
-      code: createProductDto.brand,
+    const [brands, sizes, colors, tags] = await Promise.all([
+      this.brandService.find({
+        $in: {
+          code: createProductDto.brands,
+        },
+      }),
+      this.sizeService.find({
+        code: {
+          $in: createProductDto.sizes,
+        },
+      }),
+      this.colorService.find({
+        code: {
+          $in: createProductDto.colors,
+        },
+      }),
+      this.tagService.find({
+        code: {
+          $in: createProductDto.tags,
+        },
+      }),
+    ]);
+
+    return this.repository.create({
+      ...createProductDto,
+      brands: brands.map((brand) => brand._id),
+      sizes: sizes.map((size) => size._id),
+      colors: colors.map((color) => color._id),
+      tags: tags.map((tag) => tag._id),
     });
-
-    if (!brand) {
-      this.debuggerService.error('Brand not found', 'ProductService', 'create');
-
-      throw new NotFoundException('Brand not found');
-    }
-
-    createProductDto.brand = brand._id;
-
-    const sizes = await this.sizeService.find({
-      code: {
-        $in: createProductDto.sizes,
-      },
-    });
-
-    if (sizes.length === 0) {
-      this.debuggerService.error('Sizes not found', 'ProductService', 'create');
-
-      throw new NotFoundException('Sizes not found');
-    }
-
-    createProductDto.sizes = sizes.map((size) => size._id);
-
-    const colors = await this.colorService.find({
-      code: {
-        $in: createProductDto.colors,
-      },
-    });
-
-    if (colors.length === 0) {
-      this.debuggerService.error(
-        'Colors not found',
-        'ProductService',
-        'create',
-      );
-
-      throw new NotFoundException('Colors not found');
-    }
-
-    createProductDto.colors = colors.map((color) => color._id);
-
-    return this.repository.create(createProductDto);
-  }
-
-  async findById(id: string | Types.ObjectId): Promise<IProductDocument> {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Product with id: ${id} not found`,
-        'ProductService',
-        'findById',
-      );
-
-      throw new NotFoundException(`Product with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async findOne(
-    filter: FilterQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error(
-        'Product not found',
-        'ProductService',
-        'findOne',
-      );
-
-      throw new NotFoundException('Product not found');
-    }
-
-    return result;
-  }
-
-  async findPaginated(
-    filter: FindProductsDto,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IProductDocument>> {
-    return this.repository.paginate(filter, paginateOptions);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    const result = await this.repository.updateById(id, update);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Product with id: ${id} not found`,
-        'ProductService',
-        'updateById',
-      );
-
-      throw new NotFoundException(`Product with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async deleteById(id: string | Types.ObjectId): Promise<void> {
-    const result = await this.repository.deleteById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Product with id: ${id} not found`,
-        'ProductService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Product with id: ${id} not found`);
-    }
   }
 
   async uploadMainImage(
     id: string | Types.ObjectId,
     file: Express.Multer.File,
   ): Promise<IProductDocument> {
-    const product = await this.repository.findById(id);
+    const product = await this.findById(id);
 
     if (!product) {
       this.debuggerService.error(
-        'Product not found',
+        'No data found',
         'ProductService',
-        'uploadImage',
+        'uploadMainImage',
       );
 
-      throw new NotFoundException('Product not found');
+      throw new NotFoundException('No data found');
     }
 
     if (product.mainImage) {
       const image = await this.imageService.findById(product.mainImage);
-
-      if (!image) {
-        this.debuggerService.error(
-          'Image not found',
-          'ProductService',
-          'uploadImage',
-        );
-
-        throw new NotFoundException('Image not found');
-      }
 
       const isDeleted = await this.awsService.s3DeleteItemInBucket(
         image.pathWithFilename,
@@ -237,16 +130,16 @@ export class ProductService {
     id: string | Types.ObjectId,
     files: Express.Multer.File[],
   ): Promise<IProductDocument> {
-    const product = await this.repository.findById(id);
+    const product = await this.findById(id);
 
     if (!product) {
       this.debuggerService.error(
-        'Product not found',
+        'No data found',
         'ProductService',
-        'uploadImage',
+        'uploadImages',
       );
 
-      throw new NotFoundException('Product not found');
+      throw new NotFoundException('No data found');
     }
 
     for (const file of files) {
--- src/app/modules/product/repositories/product.repository.ts
+++ src/app/modules/product/repositories/product.repository.ts
@@ -1,71 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, FilterQuery, UpdateQuery, Types } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { IProductDocument } from '../interfaces/product.interface';
 import { Product } from '../models/product.entity';
+
 @Injectable()
-export class ProductRepository {
+export class ProductRepository extends BaseRepository<IProductDocument> {
   constructor(
     @InjectModel(Product.name)
     protected readonly model: PaginateModel<IProductDocument>,
-  ) {}
-
-  async create(doc: Partial<IProductDocument>): Promise<IProductDocument> {
-    return this.model.create(doc);
-  }
-
-  async findById(id: string | Types.ObjectId): Promise<IProductDocument> {
-    return this.model.findById(id, {}, { lean: true });
-  }
-
-  async findOne(
-    filter: FilterQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(
-    filter: FilterQuery<IProductDocument>,
-  ): Promise<IProductDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async updateOne(
-    filter: FilterQuery<IProductDocument>,
-    update: UpdateQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    return this.model.findOneAndUpdate(filter, update, {
-      new: true,
-      useFindAndModify: false,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId): Promise<IProductDocument> {
-    return this.model.findByIdAndRemove(id);
-  }
-
-  async deleteOne(
-    filter: FilterQuery<IProductDocument>,
-  ): Promise<IProductDocument> {
-    return this.model.findOneAndRemove(filter, { useFindAndModify: false });
-  }
-
-  async paginate(
-    filter: FilterQuery<IProductDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IProductDocument>> {
-    return this.model.paginate(filter, paginateOptions);
+  ) {
+    super(model);
   }
 }
