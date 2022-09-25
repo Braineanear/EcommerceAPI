b7e11d2e02cb1adf58d0b91a08bca240fedd4b0d
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:30:40 +0200

    refactor(category): use base service and repository

--- src/app/modules/category/repositories/category.repository.ts
+++ src/app/modules/category/repositories/category.repository.ts
@@ -1,67 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, Types, FilterQuery, UpdateQuery } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
+import { PaginateModel,  } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { ICategoryDocument } from '../interfaces/category.interface';
 import { Category } from '../models/category.entity';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 
 @Injectable()
-export class CategoryRepository {
+export class CategoryRepository extends BaseRepository<ICategoryDocument> {
   constructor(
     @InjectModel(Category.name)
     protected readonly model: PaginateModel<ICategoryDocument>,
-  ) {}
-
-  async findById(id: string | Types.ObjectId): Promise<ICategoryDocument> {
-    return this.model.findById(id);
-  }
-
-  async findOne(
-    filter: FilterQuery<ICategoryDocument>,
-  ): Promise<ICategoryDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(
-    filter: FilterQuery<ICategoryDocument>,
-  ): Promise<ICategoryDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async findPaginated(
-    filter: FilterQuery<ICategoryDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<ICategoryDocument>> {
-    return this.model.paginate(filter, paginateOptions);
-  }
-
-  async updateOne(
-    filter: FilterQuery<ICategoryDocument>,
-    update: UpdateQuery<ICategoryDocument>,
   ) {
-    return this.model.updateOne(filter, update);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<ICategoryDocument>,
-  ) {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    return this.model.deleteOne({ _id: id });
-  }
-
-  async deleteOne(filter: FilterQuery<ICategoryDocument>) {
-    return this.model.deleteOne(filter);
-  }
-
-  async create(doc: Partial<ICategoryDocument>) {
-    return this.model.create(doc);
+    super(model);
   }
 }
