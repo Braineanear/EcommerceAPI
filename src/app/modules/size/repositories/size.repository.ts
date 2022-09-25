28a3b725bf6947361ec77a4099b7d4b81b8f59d4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:35:15 +0200

    refactor(size): use base service and repository

--- src/app/modules/size/repositories/size.repository.ts
+++ src/app/modules/size/repositories/size.repository.ts
@@ -1,63 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, Types, FilterQuery, UpdateQuery } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { ISizeDocument } from '../interfaces/size.interface';
 import { Size } from '../models/size.entity';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 
 @Injectable()
-export class SizeRepository {
+export class SizeRepository extends BaseRepository<ISizeDocument> {
   constructor(
     @InjectModel(Size.name)
     protected readonly model: PaginateModel<ISizeDocument>,
-  ) {}
-
-  async findById(id: string | Types.ObjectId): Promise<ISizeDocument> {
-    return this.model.findById(id);
-  }
-
-  async findOne(filter: FilterQuery<ISizeDocument>): Promise<ISizeDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(filter: FilterQuery<ISizeDocument>): Promise<ISizeDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async findPaginated(
-    filter: FilterQuery<ISizeDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<ISizeDocument>> {
-    return this.model.paginate(filter, paginateOptions);
-  }
-
-  async updateOne(
-    filter: FilterQuery<ISizeDocument>,
-    update: UpdateQuery<ISizeDocument>,
   ) {
-    return this.model.updateOne(filter, update);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<ISizeDocument>,
-  ) {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    return this.model.deleteOne({ _id: id });
-  }
-
-  async deleteOne(filter: FilterQuery<ISizeDocument>) {
-    return this.model.deleteOne(filter);
-  }
-
-  async create(doc: Partial<ISizeDocument>) {
-    return this.model.create(doc);
+    super(model);
   }
 }
--- src/app/modules/size/size.service.ts
+++ src/app/modules/size/size.service.ts
@@ -1,93 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Types } from 'mongoose';
-import { DebuggerService } from '@shared/debugger/debugger.service';
+import { Injectable } from '@nestjs/common';
+import { BaseService } from '@shared/services/base.service';
 import { SizeRepository } from './repositories/size.repository';
-import { CreateSizeDto } from './dtos/create-size.dto';
 
 @Injectable()
-export class SizeService {
-  constructor(
-    protected readonly repository: SizeRepository,
-    protected readonly debuggerService: DebuggerService,
-  ) {}
-
-  async findById(id: string | Types.ObjectId) {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Size with id: ${id} not found`,
-        'SizeService',
-        'findById',
-      );
-
-      throw new NotFoundException(`Size with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async findOne(filter: object) {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error('Size not found', 'SizeService', 'findOne');
-
-      throw new NotFoundException('Size not found');
-    }
-
-    return result;
-  }
-
-  async find(filter: object) {
-    const results = await this.repository.find(filter);
-
-    if (results.length === 0) {
-      this.debuggerService.error('Sizes not found', 'SizeService', 'find');
-
-      throw new NotFoundException('Sizes not found');
-    }
-
-    return results;
-  }
-
-  async findPaginated(filter: object, paginateOptions) {
-    return this.repository.findPaginated(filter, paginateOptions);
-  }
-
-  async updateById(id: string, update: object) {
-    const result = await this.repository.updateById(id, update);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Size with id: ${id} not found`,
-        'SizeService',
-        'updateById',
-      );
-
-      throw new NotFoundException(`Size with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async deleteById(id: string) {
-    const result = await this.repository.deleteById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Size with id: ${id} not found`,
-        'SizeService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Size with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async create(doc: CreateSizeDto) {
-    return this.repository.create(doc);
+export class SizeService extends BaseService<SizeRepository> {
+  constructor(protected readonly repository: SizeRepository) {
+    super();
   }
 }
