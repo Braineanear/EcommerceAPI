9fa0b0b8cbf89c0967af4c73f75a7d1dfcb13b54
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:35:03 +0200

    refactor(review): use base service and repository

--- src/app/modules/review/repositories/review.repository.ts
+++ src/app/modules/review/repositories/review.repository.ts
@@ -1,69 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, Types, FilterQuery, UpdateQuery } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { IReviewDocument } from '../interfaces/review.interface';
 import { Review } from '../models/review.entity';
 
 @Injectable()
-export class ReviewRepository {
+export class ReviewRepository extends BaseRepository<IReviewDocument> {
   constructor(
     @InjectModel(Review.name)
     protected readonly model: PaginateModel<IReviewDocument>,
-  ) {}
-
-  async findById(id: string | Types.ObjectId): Promise<IReviewDocument> {
-    return this.model.findById(id);
-  }
-
-  async findOne(
-    filter: FilterQuery<IReviewDocument>,
-  ): Promise<IReviewDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(filter: FilterQuery<IReviewDocument>): Promise<IReviewDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async findPaginated(
-    filter: FilterQuery<IReviewDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IReviewDocument>> {
-    return this.model.paginate(filter, paginateOptions);
-  }
-
-  async updateOne(
-    filter: FilterQuery<IReviewDocument>,
-    update: UpdateQuery<IReviewDocument>,
   ) {
-    return this.model.updateOne(filter, update);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IReviewDocument>,
-  ) {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    return this.model.deleteOne({ _id: id });
-  }
-
-  async deleteOne(filter: FilterQuery<IReviewDocument>) {
-    return this.model.deleteOne(filter);
-  }
-
-  async create(doc: Partial<IReviewDocument>) {
-    return this.model.create(doc);
-  }
-
-  async aggregate(pipeline: any[]): Promise<any> {
-    return this.model.aggregate(pipeline);
+    super(model);
   }
 }
--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { BaseService } from '@shared/services/base.service';
 import { DebuggerService } from '@shared/debugger/debugger.service';
 import { ProductService } from '@modules/product/product.service';
 import { ReviewRepository } from './repositories/review.repository';
@@ -6,73 +7,16 @@ import { CreateReviewDto } from './dtos/create-review.dto';
 import { Types } from 'mongoose';
 
 @Injectable()
-export class ReviewService {
+export class ReviewService extends BaseService<ReviewRepository> {
   constructor(
     protected readonly repository: ReviewRepository,
     protected readonly debuggerService: DebuggerService,
     protected readonly productService: ProductService,
-  ) {}
-
-  async findById(id: string) {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Review with id: ${id} not found`,
-        'ReviewService',
-        'findById',
-      );
-
-      throw new NotFoundException(`Review with id: ${id} not found`);
-    }
-
-    return result;
+  ) {
+    super();
   }
-
-  async findOne(filter: object) {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error(
-        'Review not found',
-        'ReviewService',
-        'findOne',
-      );
-
-      throw new NotFoundException('Review not found');
-    }
-
-    return result;
-  }
-
-  async find(filter: object) {
-    const results = await this.repository.find(filter);
-
-    if (results.length === 0) {
-      this.debuggerService.error('Reviews not found', 'ReviewService', 'find');
-
-      throw new NotFoundException('Reviews not found');
-    }
-
-    return results;
-  }
-
-  async findPaginated(filter: object, paginateOptions) {
-    return this.repository.findPaginated(filter, paginateOptions);
-  }
-
   async updateById(id: string, update: object) {
-    const review = await this.repository.findById(id);
-
-    if (!review) {
-      this.debuggerService.error(
-        `Review with id: ${id} not found`,
-        'ReviewService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Review with id: ${id} not found`);
-    }
+    const review = await this.findById(id);
 
     const stats = await this.repository.aggregate([
       {
@@ -103,17 +47,7 @@ export class ReviewService {
   }
 
   async deleteById(id: string) {
-    const review = await this.repository.findById(id);
-
-    if (!review) {
-      this.debuggerService.error(
-        `Review with id: ${id} not found`,
-        'ReviewService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Review with id: ${id} not found`);
-    }
+    const review = await this.findById(id);
 
     const stats = await this.repository.aggregate([
       {
