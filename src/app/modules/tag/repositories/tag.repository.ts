1f8d32e558e4ba013af9a828c2ed40fb68db9cf7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:35:26 +0200

    refactor(tag): use base service and repository

--- src/app/modules/tag/repositories/tag.repository.ts
+++ src/app/modules/tag/repositories/tag.repository.ts
@@ -1,63 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, Types, FilterQuery, UpdateQuery } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { ITagDocument } from '../interfaces/tag.interface';
 import { Tag } from '../models/tag.entity';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 
 @Injectable()
-export class TagRepository {
+export class TagRepository extends BaseRepository<ITagDocument> {
   constructor(
     @InjectModel(Tag.name)
     protected readonly model: PaginateModel<ITagDocument>,
-  ) {}
-
-  async findById(id: string | Types.ObjectId): Promise<ITagDocument> {
-    return this.model.findById(id);
-  }
-
-  async findOne(filter: FilterQuery<ITagDocument>): Promise<ITagDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(filter: FilterQuery<ITagDocument>): Promise<ITagDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async findPaginated(
-    filter: FilterQuery<ITagDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<ITagDocument>> {
-    return this.model.paginate(filter, paginateOptions);
-  }
-
-  async updateOne(
-    filter: FilterQuery<ITagDocument>,
-    update: UpdateQuery<ITagDocument>,
   ) {
-    return this.model.updateOne(filter, update);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<ITagDocument>,
-  ) {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    return this.model.deleteOne({ _id: id });
-  }
-
-  async deleteOne(filter: FilterQuery<ITagDocument>) {
-    return this.model.deleteOne(filter);
-  }
-
-  async create(doc: Partial<ITagDocument>) {
-    return this.model.create(doc);
+    super(model);
   }
 }
--- src/app/modules/tag/tag.service.ts
+++ src/app/modules/tag/tag.service.ts
@@ -1,93 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Types } from 'mongoose';
-import { DebuggerService } from '@shared/debugger/debugger.service';
+import { Injectable } from '@nestjs/common';
+import { BaseService } from '@shared/services/base.service';
 import { TagRepository } from './repositories/tag.repository';
-import { CreateTagDto } from './dtos/create-tag.dto';
 
 @Injectable()
-export class TagService {
-  constructor(
-    protected readonly repository: TagRepository,
-    protected readonly debuggerService: DebuggerService,
-  ) {}
-
-  async findById(id: string | Types.ObjectId) {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Tag with id: ${id} not found`,
-        'TagService',
-        'findById',
-      );
-
-      throw new NotFoundException(`Tag with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async findOne(filter: object) {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error('Tag not found', 'TagService', 'findOne');
-
-      throw new NotFoundException('Tag not found');
-    }
-
-    return result;
-  }
-
-  async find(filter: object) {
-    const results = await this.repository.find(filter);
-
-    if (results.length === 0) {
-      this.debuggerService.error('Tags not found', 'TagService', 'find');
-
-      throw new NotFoundException('Tags not found');
-    }
-
-    return results;
-  }
-
-  async findPaginated(filter: object, paginateOptions) {
-    return this.repository.findPaginated(filter, paginateOptions);
-  }
-
-  async updateById(id: string, update: object) {
-    const result = await this.repository.updateById(id, update);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Tag with id: ${id} not found`,
-        'TagService',
-        'updateById',
-      );
-
-      throw new NotFoundException(`Tag with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async deleteById(id: string) {
-    const result = await this.repository.deleteById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Tag with id: ${id} not found`,
-        'TagService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Tag with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async create(doc: CreateTagDto) {
-    return this.repository.create(doc);
+export class TagService extends BaseService<TagRepository> {
+  constructor(protected readonly repository: TagRepository) {
+    super();
   }
 }
