3ae585d1056aec64c98f7d314d1bd6493dbeb41c
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:30:58 +0200

    refactor(image): use base service and repository

--- src/app/modules/image/image.service.ts
+++ src/app/modules/image/image.service.ts
@@ -1,93 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Types } from 'mongoose';
-import { DebuggerService } from '@shared/debugger/debugger.service';
+import { Injectable } from '@nestjs/common';
+import { BaseService } from '@shared/services/base.service';
 import { ImageRepository } from './repositories/image.repository';
-import { CreateImageDto } from './dtos/create-image.dto';
 
 @Injectable()
-export class ImageService {
-  constructor(
-    protected repository: ImageRepository,
-    protected debuggerService: DebuggerService,
-  ) {}
-
-  async findById(id: string | Types.ObjectId) {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Image with id ${id} not found`,
-        'ImageService',
-        'findById',
-      );
-
-      throw new NotFoundException(`Image with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async findOne(filter: object) {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error('Image not found', 'ImageService', 'findOne');
-
-      throw new NotFoundException('Image not found');
-    }
-
-    return result;
-  }
-
-  async find(filter: object) {
-    const results = await this.repository.find(filter);
-
-    if (results.length === 0) {
-      this.debuggerService.error('Images not found', 'ImageService', 'find');
-
-      throw new NotFoundException('Images not found');
-    }
-
-    return results;
-  }
-
-  async findPaginated(filter: object, paginateOptions) {
-    return this.repository.findPaginated(filter, paginateOptions);
-  }
-
-  async updateById(id: string | Types.ObjectId, update: object) {
-    const result = await this.repository.updateById(id, update);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Image with id: ${id} not found`,
-        'ImageService',
-        'updateById',
-      );
-
-      throw new NotFoundException(`Image with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    const result = await this.repository.deleteById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `Image with id: ${id} not found`,
-        'ImageService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`Image with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async create(doc: CreateImageDto) {
-    return this.repository.create(doc);
+export class ImageService extends BaseService<ImageRepository> {
+  constructor(protected repository: ImageRepository) {
+    super();
   }
 }
--- src/app/modules/image/repositories/image.repository.ts
+++ src/app/modules/image/repositories/image.repository.ts
@@ -1,63 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, Types, FilterQuery, UpdateQuery } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { IImageDocument } from '../interfaces/image.interface';
 import { Image } from '../models/image.entity';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 
 @Injectable()
-export class ImageRepository {
+export class ImageRepository extends BaseRepository<IImageDocument> {
   constructor(
     @InjectModel(Image.name)
     protected readonly model: PaginateModel<IImageDocument>,
-  ) {}
-
-  async findById(id: string | Types.ObjectId): Promise<IImageDocument> {
-    return this.model.findById(id);
-  }
-
-  async findOne(filter: FilterQuery<IImageDocument>): Promise<IImageDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(filter: FilterQuery<IImageDocument>): Promise<IImageDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async findPaginated(
-    filter: FilterQuery<IImageDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IImageDocument>> {
-    return this.model.paginate(filter, paginateOptions);
-  }
-
-  async updateOne(
-    filter: FilterQuery<IImageDocument>,
-    update: UpdateQuery<IImageDocument>,
   ) {
-    return this.model.updateOne(filter, update);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IImageDocument>,
-  ) {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId) {
-    return this.model.deleteOne({ _id: id });
-  }
-
-  async deleteOne(filter: FilterQuery<IImageDocument>) {
-    return this.model.deleteOne(filter);
-  }
-
-  async create(doc: Partial<IImageDocument>) {
-    return this.model.create(doc);
+    super(model);
   }
 }
