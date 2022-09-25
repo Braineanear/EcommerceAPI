40ea203018aba010a663b1eb9ed9fb9f3b8c05c4
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2022-09-26 01:35:45 +0200

    refactor(user): use base service and repository

--- src/app/modules/user/repositories/user.repository.ts
+++ src/app/modules/user/repositories/user.repository.ts
@@ -1,65 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { PaginateModel, FilterQuery, UpdateQuery, Types } from 'mongoose';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
+import { PaginateModel } from 'mongoose';
+import { BaseRepository } from '@shared/repositories/base.repository';
 import { IUserDocument } from '../interfaces/user.interface';
 import { User } from '../models/user.entity';
 @Injectable()
-export class UserRepository {
+export class UserRepository extends BaseRepository<IUserDocument> {
   constructor(
     @InjectModel(User.name)
     protected readonly model: PaginateModel<IUserDocument>,
-  ) {}
-
-  async create(doc: Partial<IUserDocument>): Promise<IUserDocument> {
-    return this.model.create(doc);
-  }
-
-  async findById(id: string | Types.ObjectId): Promise<IUserDocument> {
-    return this.model.findById(id, {}, { lean: true });
-  }
-
-  async findOne(filter: FilterQuery<IUserDocument>): Promise<IUserDocument> {
-    return this.model.findOne(filter);
-  }
-
-  async find(filter: FilterQuery<IUserDocument>): Promise<IUserDocument[]> {
-    return this.model.find(filter);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IUserDocument>,
-  ): Promise<IUserDocument> {
-    return this.model.findByIdAndUpdate(id, update, {
-      new: true,
-      useFindAndModify: true,
-    });
-  }
-
-  async updateOne(
-    filter: FilterQuery<IUserDocument>,
-    update: UpdateQuery<IUserDocument>,
-  ): Promise<IUserDocument> {
-    return this.model.findOneAndUpdate(filter, update, {
-      new: true,
-      useFindAndModify: false,
-    });
-  }
-
-  async deleteById(id: string | Types.ObjectId): Promise<IUserDocument> {
-    return this.model.findByIdAndRemove(id);
-  }
-
-  async deleteOne(filter: FilterQuery<IUserDocument>): Promise<IUserDocument> {
-    return this.model.findOneAndRemove(filter, { useFindAndModify: false });
-  }
-
-  async paginate(
-    filter: FilterQuery<IUserDocument>,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IUserDocument>> {
-    return this.model.paginate(filter, paginateOptions);
+  ) {
+    super(model);
   }
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,115 +3,33 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { FilterQuery, UpdateQuery, Types } from 'mongoose';
+import { Types } from 'mongoose';
+import { BaseService } from '@shared/services/base.service';
 import { DebuggerService } from '@shared/debugger/debugger.service';
-import { IPaginateOptions } from '@shared/interfaces/i-paginate-options';
 import { AwsS3Service } from '@shared/aws/aws.service';
 import { IAwsS3Response } from '@shared/aws/interfaces/aws.interface';
 import { ImageService } from '@modules/image/image.service';
 import { UserRepository } from './repositories/user.repository';
-import { FindUsersDto } from './dtos/find-users.dto';
 import { IUserDocument } from './interfaces/user.interface';
-import { IPaginatedInterface } from '@shared/interfaces/i-paginate-result.interface';
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService<UserRepository> {
   constructor(
     protected readonly repository: UserRepository,
     protected readonly debuggerService: DebuggerService,
     protected readonly awsService: AwsS3Service,
     protected readonly imageService: ImageService,
-  ) {}
-
-  create(createUserDto: Partial<IUserDocument>): Promise<IUserDocument> {
-    return this.repository.create(createUserDto);
-  }
-
-  async findById(id: string | Types.ObjectId): Promise<IUserDocument> {
-    const result = await this.repository.findById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `User with id: ${id} not found`,
-        'UserService',
-        'findById',
-      );
-
-      throw new NotFoundException(`User with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async findOne(filter: FilterQuery<IUserDocument>): Promise<IUserDocument> {
-    const result = await this.repository.findOne(filter);
-
-    if (!result) {
-      this.debuggerService.error('User not found', 'UserService', 'findOne');
-
-      throw new NotFoundException('User not found');
-    }
-
-    return result;
-  }
-
-  async findPaginated(
-    filter: FindUsersDto,
-    paginateOptions: IPaginateOptions,
-  ): Promise<IPaginatedInterface<IUserDocument>> {
-    return this.repository.paginate(filter, paginateOptions);
-  }
-
-  async updateById(
-    id: string | Types.ObjectId,
-    update: UpdateQuery<IUserDocument>,
-  ): Promise<IUserDocument> {
-    const result = await this.repository.updateById(id, update);
-
-    if (!result) {
-      this.debuggerService.error(
-        `User with id: ${id} not found`,
-        'UserService',
-        'updateById',
-      );
-
-      throw new NotFoundException(`User with id: ${id} not found`);
-    }
-
-    return result;
-  }
-
-  async deleteById(id: string | Types.ObjectId): Promise<void> {
-    const result = await this.repository.deleteById(id);
-
-    if (!result) {
-      this.debuggerService.error(
-        `User with id: ${id} not found`,
-        'UserService',
-        'deleteById',
-      );
-
-      throw new NotFoundException(`User with id: ${id} not found`);
-    }
+  ) {
+    super();
   }
 
   async uploadImage(
     id: string | Types.ObjectId,
     file: Express.Multer.File,
   ): Promise<IUserDocument> {
-    const user = await this.repository.findOne({
+    const user = await this.findOne({
       _id: id,
     });
-
-    if (!user) {
-      this.debuggerService.error(
-        'User not found',
-        'UserService',
-        'uploadImage',
-      );
-
-      throw new NotFoundException('User not found');
-    }
 
     if (user.avatar) {
       const image = await this.imageService.findById(user.avatar);
