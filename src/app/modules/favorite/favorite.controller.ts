1e1658450d8897742ba219ed0b4f31ffeb519f14
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2023-07-29 16:16:41 +0330

    feat(docs): add swagger docs

--- src/app/modules/favorite/favorite.controller.ts
+++ src/app/modules/favorite/favorite.controller.ts
@@ -1,5 +1,26 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+  ApiBadRequestResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 import { AuthUser } from '@shared/decorators/auth-user.decorator';
 import { Roles } from '@shared/decorators/roles.decorator';
 import { RoleTypeEnum } from '@shared/enums/role-type.enum';
@@ -18,6 +39,10 @@ export class FavoriteController {
   constructor(private readonly service: FavoriteService) {}
 
   @Get()
+  @ApiOkResponse({ description: 'Successfully fetched favorites.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @ApiOperation({ summary: 'Get all favorites' })
+  @ApiQuery({ type: FindFavoritesDto, description: 'Pagination options' })
   findAll(
     @Query(new PaginationPipe()) q: FindFavoritesDto,
     @AuthUser() user: JwtPayload,
@@ -32,16 +57,34 @@ export class FavoriteController {
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Successfully fetched favorite.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @ApiNotFoundResponse({ description: 'Favorite not found.' })
+  @ApiOperation({ summary: 'Get favorite by ID' })
+  @ApiParam({ name: 'id', type: 'string', description: 'Favorite ID' })
   findById(@Param('id') id: string) {
     return this.service.findById(id);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'Successfully deleted favorite.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiNotFoundResponse({ description: 'Favorite not found.' })
+  @ApiOperation({ summary: 'Delete favorite by ID' })
+  @ApiParam({ name: 'id', type: 'string', description: 'Favorite ID' })
   deleteById(@Param('id') id: string) {
     return this.service.deleteById(id);
   }
 
   @Post()
+  @ApiCreatedResponse({ description: 'Successfully created favorite.' })
+  @ApiBadRequestResponse({ description: 'Bad Request.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiOperation({ summary: 'Create favorite' })
+  @ApiBody({ type: CreateFavoriteDto, description: 'Favorite Information' })
   create(@Body() data: CreateFavoriteDto, @AuthUser() user: JwtPayload) {
     return this.service.create(data, user.sub);
   }
